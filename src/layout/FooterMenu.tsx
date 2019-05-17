414fbc69e72e965aeee22cd6ee3cb297876783dc
Tom Bean <tomcbean@gmail.com>
2019-05-16 23:32:45 -0400

    added stats bottom link

--- src/layout/FooterMenu.tsx
+++ src/layout/FooterMenu.tsx
@@ -1,17 +1,21 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export class FooterMenu extends Component {
   public render() {
     return (
       <div className="footer-menu">
         <div className="footer-menu__item">
-          Powered by <a href="//bzx.network">bZx</a>
-        </div>
-        <div className="footer-menu__item">
           <a href="//fulcrum.trade/tos/">Terms of use</a>
         </div>
         <div className="footer-menu__item">
           <a href="//fulcrum.trade/privacy/">Privacy policy</a>
+        </div>
+        <Link className="footer-menu__item" to="/stats">
+          Stats
+        </Link>
+        <div className="footer-menu__item">
+          Powered by <a href="//bzx.network">bZx</a>
         </div>
       </div>
     );
