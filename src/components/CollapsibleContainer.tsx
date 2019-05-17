38e5baed74abc339e98868d136deb9ff138fad47
Alex Shafranovich <alexey.shafranovich@live.com>
2019-05-18 00:05:48 +0300

    reusability fix for CollapsibleContainer

--- src/components/CollapsibleContainer.tsx
+++ src/components/CollapsibleContainer.tsx
@@ -6,10 +6,12 @@ export interface ICollapsibleContainerProps {
 
 export class CollapsibleContainer extends Component<ICollapsibleContainerProps> {
   public render() {
+    const uuid = this.uuidv4();
+
     return (
       <div className="collapsible-container">
-        <input id="collapsible" className="collapsible-container__toggle-cb" type="checkbox" />
-        <label htmlFor="collapsible" className="collapsible-container__toggle-label">
+        <input id={uuid} className="collapsible-container__toggle-cb" type="checkbox" />
+        <label htmlFor={uuid} className="collapsible-container__toggle-label">
           {this.props.title}
         </label>
         <div className="collapsible-container__content">
@@ -17,5 +19,14 @@ export class CollapsibleContainer extends Component<ICollapsibleContainerProps> 
         </div>
       </div>
     );
+  }
+
+  // https://gist.github.com/jed/982883
+  private uuidv4 = () => {
+    // @ts-ignore
+    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+      // tslint:disable-next-line:no-bitwise
+      (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    )
   }
 }
