3b61258f6eca780559bb6560464c55dee979d2da
Boris Shevchenko <bshevchenkoweb@gmail.com>
2019-10-28 17:06:09 +0300

    profit ticker

---
+++ packages/fulcrum/src/components/ProfitTicker.tsx
@@ -0,0 +1,37 @@
+import { BigNumber } from "@0x/utils";
+import React, { Component } from "react";
+
+export interface IProfitTickerProps {
+  secondDiff: number;
+  profit: BigNumber | null;
+}
+
+export class ProfitTicker extends Component<IProfitTickerProps> {
+
+  private readonly container: React.RefObject<any>;
+
+  constructor(props: IProfitTickerProps) {
+    super(props);
+    this.container = React.createRef();
+  }
+
+  public componentDidMount(): void {
+    const ms = 50;
+    const diff = this.props.secondDiff / (1000 / ms);
+    let value = this.props.profit ? this.props.profit.toNumber() : 0;
+    setInterval(() => {
+      value = value + diff;
+      this.container.current.innerHTML = "$" + value.toFixed(8);
+    }, ms);
+  }
+
+  public render() {
+    const { profit } = this.props;
+    return ( // TODO @bshevchenko: return title attr back
+      <div
+        ref={this.container}
+        className="token-selector-item__profit-value"
+      >${profit ? profit.toFixed(8) : ''}</div>
+    );
+  }
+}
