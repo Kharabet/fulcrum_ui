407b8744c1a063a572dcdae1f85deec38ea1b7d1
KevalS <keval688@gmail.com>
2019-12-07 11:22:54 +0530

    trade page for fulcrum

---
+++ packages/fulcrum/src/components/TradeTokenGridHeaderMobile.tsx
@@ -0,0 +1,50 @@
+import React, { ChangeEvent, Component } from "react";
+
+
+export interface ITradeTokenGridHeaderProps {
+  showMyTokensOnly: boolean;
+  onShowMyTokensOnlyChange: (value: boolean) => void;
+}
+
+export class TradeTokenGridHeaderMobile extends Component<ITradeTokenGridHeaderProps> {
+
+  public render() {
+    return (
+      <div>
+
+        <div className="trade-token-grid-header">
+
+          <div className="trade-token-grid-header__col-leverage">
+            <span className="trade-token-grid-header__text">Leverage</span>
+          </div>
+          <div className="trade-token-grid-header__col-price">
+            <span className="trade-token-grid-header__text">Price</span>
+          </div>
+          {/*<div className="trade-token-grid-header__col-price">*/}
+            {/*<span className="trade-token-grid-header__text">Liquidation Price</span>*/}
+          {/*</div>*/}
+          <div className="trade-token-grid-header__col-change24h">
+            <span className="trade-token-grid-header__text">24 hours</span>
+          </div>
+          <div className="trade-token-grid-header__col-profit">
+            <span className="trade-token-grid-header__text">Profit</span>
+          </div>
+          <div className="trade-token-grid-header__col-profit">
+            <span className="trade-token-grid-header__text"></span>
+          </div>
+          {/*<div className="trade-token-grid-header__col-actions">*/}
+            {/*<span className="trade-token-grid-header__text-right">*/}
+              {/*<CheckBox checked={this.props.showMyTokensOnly} onChange={this.showMyTokensOnlyChange}>Manage Positions</CheckBox>*/}
+            {/*</span>*/}
+          {/*</div>*/}
+        </div>
+      </div>
+    );
+  }
+
+
+
+  public showMyTokensOnlyChange = (event: ChangeEvent<HTMLInputElement>) => {
+    this.props.onShowMyTokensOnlyChange(event.target.checked);
+  }
+}
