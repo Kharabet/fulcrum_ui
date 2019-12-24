1fd289dec7fd2ce4d53893c9820d6b0465dc85f9
Rodion Kharabet <pensioneer4ik@gmail.com>
2019-12-24 18:14:38 +0200

    Remove chart.js annotations plugin after merge

--- packages/fulcrum-website/assets/js/main.js
+++ packages/fulcrum-website/assets/js/main.js
@@ -15,20 +15,11 @@ window.addEventListener('load', function () {
         if (e.target.checked) {
             document.documentElement.setAttribute('data-theme', 'dark');
             localStorage.setItem('theme', 'dark');
-
-            if (window.chart)
-                chart.options.annotation.annotations[0].borderColor = '#ffffff';
         }
         else {
             document.documentElement.setAttribute('data-theme', 'light');
             localStorage.setItem('theme', 'light');
-
-            if (window.chart)
-                chart.options.annotation.annotations[0].borderColor = '#495460';
         }
-
-        if (window.chart)
-            chart.update();
     }
 
     toggleSwitch.addEventListener('change', switchTheme, false);
