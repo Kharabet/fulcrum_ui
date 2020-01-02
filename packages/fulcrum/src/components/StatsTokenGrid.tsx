f3d1d10e513734206b1e8f4fdaac6f1a278002c2
Tom Bean <tomcbean@gmail.com>
2020-01-02 14:22:12 -0500

    Stats page updated for efficient queries

--- packages/fulcrum/src/components/StatsTokenGrid.tsx
+++ packages/fulcrum/src/components/StatsTokenGrid.tsx
@@ -1,15 +1,21 @@
+import { BigNumber } from "@0x/utils";
 import React, { Component } from "react";
 import { Asset } from "../domain/Asset";
 import { StatsTokenGridCard } from "./StatsTokenGridCard";
 import { StatsTokenGridHeader } from "./StatsTokenGridHeader";
+import { ReserveDetails } from "../domain/ReserveDetails";
 import { IStatsTokenGridRowProps, StatsTokenGridRow } from "./StatsTokenGridRow";
+import { FulcrumProviderEvents } from "../services/events/FulcrumProviderEvents";
+import { FulcrumProvider } from "../services/FulcrumProvider";
 
 export interface IStatsTokenGridProps {
   isMobileMedia: boolean;
 }
 
 interface IStatsTokenGridState {
-  tokenRowsData: IStatsTokenGridRowProps[];
+  tokenRowsData: IStatsTokenGridRowProps[] | null;
+  totalsRow: IStatsTokenGridRowProps | null;
+  isLoaded: boolean;
 }
 
 export class StatsTokenGrid extends Component<IStatsTokenGridProps, IStatsTokenGridState> {
@@ -32,42 +38,84 @@ export class StatsTokenGrid extends Component<IStatsTokenGridProps, IStatsTokenG
     super(props);
 
     this.state = {
-      tokenRowsData: StatsTokenGrid.getRowsData(props)
+      tokenRowsData: null,
+      totalsRow: null,
+      isLoaded: false
     };
+
+    FulcrumProvider.Instance.eventEmitter.on(FulcrumProviderEvents.ProviderAvailable, this.onProviderAvailable);
   }
 
   public async derivedUpdate() {
-    this.setState({ ...this.state, tokenRowsData: StatsTokenGrid.getRowsData(this.props) });
+    const reserveDetails = await FulcrumProvider.Instance.getReserveDetails(StatsTokenGrid.assets);
+    //console.log(reserveDetails);
+
+    const rowData = await StatsTokenGrid.getRowsData(reserveDetails);
+    let totalsRow: IStatsTokenGridRowProps | null = null;
+    if (rowData.length > 0) {
+      totalsRow = rowData.pop()!;
+    }
+
+    this.setState({ 
+      ...this.state,
+      tokenRowsData: rowData,
+      totalsRow: totalsRow,
+      isLoaded: true
+    });
+  }
+
+  private onProviderAvailable = () => {
+    this.derivedUpdate();
+  };
+
+  public componentWillUnmount(): void {
+    FulcrumProvider.Instance.eventEmitter.removeListener(FulcrumProviderEvents.ProviderAvailable, this.onProviderAvailable);
   }
 
   public componentDidMount(): void {
     this.derivedUpdate();
   }
 
-  /*public componentWillUnmount(): void {
-  }*/
-
   public render() {
-    const tokenRows = !this.props.isMobileMedia
-      ? this.state.tokenRowsData.map(e => <StatsTokenGridRow key={e.asset} {...e} />)
-      : this.state.tokenRowsData.map(e => <StatsTokenGridCard key={e.asset} {...e} />);
+    if (!this.state.tokenRowsData || !this.state.totalsRow) {
+      return (
+        <div className="stats-grid">
+          <StatsTokenGridHeader />
+        </div>
+      );
+    }
+
+    let tokenRows;
+    let totalsRow;
+    if (this.props.isMobileMedia) {
+      tokenRows = this.state.tokenRowsData.map(e => <StatsTokenGridCard key={e.reserveDetails.asset!} {...e} />);
+      totalsRow = (<StatsTokenGridCard key={this.state.totalsRow.reserveDetails.asset!} {...this.state.totalsRow} />);
+    } else {
+      tokenRows = this.state.tokenRowsData.map(e => <StatsTokenGridRow key={e.reserveDetails.asset!} {...e} />);
+      totalsRow = (<StatsTokenGridRow key={this.state.totalsRow.reserveDetails.asset!} {...this.state.totalsRow} />);
+    }
 
     return !this.props.isMobileMedia ? (
       <div className="stats-grid">
         <StatsTokenGridHeader />
         {tokenRows}
+        {totalsRow}
       </div>
     ) : (
-      <div className="stats-grid">{tokenRows}</div>
+      <div className="stats-grid">
+        {totalsRow}
+        {tokenRows}
+      </div>
     );
   }
 
-  private static getRowsData = (props: IStatsTokenGridProps): IStatsTokenGridRowProps[] => {
+  private static getRowsData = async (reserveDetails: ReserveDetails[]): Promise<IStatsTokenGridRowProps[]> => {
     const rowsData: IStatsTokenGridRowProps[] = [];
 
-    StatsTokenGrid.assets.forEach(e => {
+    // console.log(reserveDetails);
+    reserveDetails.forEach(e => {
       rowsData.push({
-        asset: e
+        reserveDetails: e
       });
     });
 
