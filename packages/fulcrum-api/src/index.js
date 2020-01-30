8baa26b48682009e39dcfbbfc92d2e11af9a2935
Rodion Kharabet <pensioneer4ik@gmail.com>
2020-01-30 21:37:44 +0200

    Add source code

---
+++ packages/fulcrum-api/src/index.js
@@ -0,0 +1,34 @@
+import http from 'http';
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
+import api from './api';
+import config from './config.json';
+
+let app = express();
+app.server = http.createServer(app);
+
+// logger
+app.use(morgan('dev'));
+
+// 3rd party middleware
+app.use(cors({
+    exposedHeaders: config.corsHeaders
+}));
+
+app.use(bodyParser.json({
+    limit: config.bodyLimit
+}));
+
+// api router
+app.use('/api', api({ config }));
+
+app.server.listen({
+    port: process.env.PORT || config.port,
+    host: '0.0.0.0'
+}, () => {
+    console.log(`Started on port ${app.server.address().port}`);
+});
+
+export default app;
