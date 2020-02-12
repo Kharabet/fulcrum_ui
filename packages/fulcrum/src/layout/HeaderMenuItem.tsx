3920bde4d4c235dcd4c9e112049242c67eb5154b
Rodion Kharabet <pensioneer4ik@gmail.com>
2020-02-12 13:59:04 +0200

    Update styles for header menu items

--- packages/fulcrum/src/layout/HeaderMenuItem.tsx
+++ packages/fulcrum/src/layout/HeaderMenuItem.tsx
@@ -15,16 +15,10 @@ export class HeaderMenuItem extends Component<IHeaderMenuItemProps> {
         {this.props.external ? (
           <a href={this.props.link} className="header-menu__item-link">
             <div>{this.props.title}</div>
-            <div className="header-menu__item-link__accent-container">
-              <div className="header-menu__item-link__accent" />
-            </div>
           </a>
         ) : (
           <NavLink to={this.props.link} exact={true} activeClassName="header-menu__item-link--active">
             <div>{this.props.title}</div>
-            <div className="header-menu__item-link__accent-container">
-              <div className="header-menu__item-link__accent" />
-            </div>
           </NavLink>
         )}
       </div>
