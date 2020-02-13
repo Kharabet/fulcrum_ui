6febece078e67902e290b81bf9c5b75c5fb4b453
Diana Kovalyova <dianaborysivna@gmail.com>
2020-02-13 11:30:27 +0200

    Update popup. Add logo as reactComponent

--- packages/fulcrum/src/domain/ProviderTypeDetails.ts
+++ packages/fulcrum/src/domain/ProviderTypeDetails.ts
@@ -1,9 +1,12 @@
 export class ProviderTypeDetails {
   public displayName: string = "";
   public logoSvg: any = null;
+  public reactLogoSvg: any = null;
 
-  constructor(displayName: string, logoSvg: any) {
+
+  constructor(displayName: string, logoSvg: any, reactLogoSvg: any) {
     this.displayName = displayName;
     this.logoSvg = logoSvg;
+    this.reactLogoSvg = reactLogoSvg;
   }
 }
--- packages/fulcrum/src/domain/ProviderTypeDictionary.ts
+++ packages/fulcrum/src/domain/ProviderTypeDictionary.ts
@@ -1,21 +1,21 @@
 import { ProviderType } from "./ProviderType";
 import { ProviderTypeDetails } from "./ProviderTypeDetails";
 
-import bitski_logo from "../assets/images/logo_big___bitski.svg";
-import fortmatic_logo from "../assets/images/logo_big___fortmatic.svg";
-import metamask_logo from "../assets/images/logo_big___metamask.svg";
-import portis_logo from "../assets/images/logo_big___portis.svg";
-import squarelink_logo from "../assets/images/logo_big___squarelink.svg";
-import walletconnect_logo from "../assets/images/logo_big___wallectconnect.svg";
+import bitski_logo, { ReactComponent as BitskiLogo } from '../assets/images/logo_big___bitski.svg';
+import fortmatic_logo, { ReactComponent as FortmaticLogo } from '../assets/images/logo_big___fortmatic.svg';
+import metamask_logo, { ReactComponent as MetamaskLogo } from '../assets/images/logo_big___metamask.svg';
+import portis_logo, { ReactComponent as PortisLogo } from '../assets/images/logo_big___portis.svg';
+import squarelink_logo, { ReactComponent as SquarelinkLogo } from '../assets/images/logo_big___squarelink.svg';
+import walletconnect_logo, { ReactComponent as WalletconnectLogo } from '../assets/images/logo_big___wallectconnect.svg';
 
 export class ProviderTypeDictionary {
   public static providerTypes: Map<ProviderType, ProviderTypeDetails> = new Map<ProviderType, ProviderTypeDetails>([
-    [ProviderType.MetaMask, new ProviderTypeDetails("MetaMask", metamask_logo)],
-    [ProviderType.Bitski, new ProviderTypeDetails("Bitski", bitski_logo)],
-    [ProviderType.Fortmatic, new ProviderTypeDetails("Fortmatic", fortmatic_logo)],
-    [ProviderType.WalletConnect, new ProviderTypeDetails("WalletConnect", walletconnect_logo)],
-    [ProviderType.Portis, new ProviderTypeDetails("Portis", portis_logo)],
-    [ProviderType.Squarelink, new ProviderTypeDetails("Squarelink", squarelink_logo)],
-    [ProviderType.None, new ProviderTypeDetails("None", null)]
+    [ProviderType.MetaMask, new ProviderTypeDetails("MetaMask", metamask_logo, MetamaskLogo)],
+    [ProviderType.Bitski, new ProviderTypeDetails("Bitski", bitski_logo, BitskiLogo)],
+    [ProviderType.Fortmatic, new ProviderTypeDetails("Fortmatic", fortmatic_logo, FortmaticLogo)],
+    [ProviderType.WalletConnect, new ProviderTypeDetails("WalletConnect", walletconnect_logo, WalletconnectLogo)],
+    [ProviderType.Portis, new ProviderTypeDetails("Portis", portis_logo, PortisLogo)],
+    [ProviderType.Squarelink, new ProviderTypeDetails("Squarelink", squarelink_logo, SquarelinkLogo)],
+    [ProviderType.None, new ProviderTypeDetails("None", null, null)]
   ]);
 }
