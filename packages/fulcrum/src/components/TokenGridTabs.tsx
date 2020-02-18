fb8806a25c3d6df97191bcf73e182b03d6c3bd96
Rodion Kharabet <pensioneer4ik@gmail.com>
2020-02-18 13:25:09 +0200

    Basic DropdownSelect component implementation

--- packages/fulcrum/src/components/TokenGridTabs.tsx
+++ packages/fulcrum/src/components/TokenGridTabs.tsx
@@ -49,7 +49,7 @@ export class TokenGridTabs extends Component<ITokenGridTabsProps, ITokenGridTabs
 
     const classNamePrefix = "trade-token-grid-tab-item";
 
-    
+
 
 
     return (
@@ -74,6 +74,17 @@ export class TokenGridTabs extends Component<ITokenGridTabsProps, ITokenGridTabs
     await this.props.onSelect(this.getTradeTokenGridRowSelectionKey(asset));
   };
 
+  private async onDropdownSelect(value: string) {
+    if (value === "manage") {
+      this.showMyTokensOnlyChange();
+      return;
+    }
+    const asset = value as Asset;
+
+    await this.props.onShowMyTokensOnlyChange(false);
+    await this.props.onSelect(this.getTradeTokenGridRowSelectionKey(asset));
+  }
+
 
   public render() {
     var selectedAsset = AssetsDictionary.assets.get(this.props.selectedKey.asset);
@@ -82,6 +93,9 @@ export class TokenGridTabs extends Component<ITokenGridTabsProps, ITokenGridTabs
     return (
       <div className={`trade-token-grid-tab ${displayName && !this.state.isShowMyTokensOnly ? displayName.toLowerCase() : "manage"}`} >
         <div className="trade-token-grid-tab__container">
+          <div className="trade-token-grid-tab__selector">
+            {this.renderDropdown()}
+          </div>
           {this.props.assets.map(asset => (this.renderAsset(asset)))}
           <div className={`trade-token-grid-tab-item ${this.state.isShowMyTokensOnly ? "trade-token-grid-tab-item--active" : ""}`} onClick={this.showMyTokensOnlyChange}>
             <div className={`trade-token-grid-tab-item__col-token-image wallet-img-div`} >
@@ -113,6 +127,59 @@ export class TokenGridTabs extends Component<ITokenGridTabsProps, ITokenGridTabs
 
   private getTradeTokenGridRowSelectionKey(asset: Asset) {
     return this.getTradeTokenGridRowSelectionKeyRaw(asset);
+  }
+
+  private onStyledSelectClick(e: React.MouseEvent<HTMLElement>) {
+    e.stopPropagation();
+    const select = e.currentTarget.closest(".select") as HTMLElement;
+    const ul = select.querySelector("ul.select-options") as HTMLElement;
+    const selectStyled = e.currentTarget;
+
+    if (selectStyled.classList.contains("active")) {
+      selectStyled.classList.remove("active");
+      ul.style.display = 'none';
+    } else {
+      selectStyled.classList.add("active");
+      ul.style.display = 'block';
+    }
+  }
+  private onLiClick(e: React.MouseEvent<HTMLElement>) {
+    e.stopPropagation();
+    const li = e.currentTarget;
+    const select = e.currentTarget.closest(".select") as HTMLElement;
+    const ul = select.querySelector("ul.select-options") as HTMLElement;
+    const selectStyled = select.querySelector(".styled-select") as HTMLElement;
+    const selectNative = select.querySelector("select[name='tabs_selector']") as HTMLSelectElement
+    selectStyled.textContent = li.textContent
+    selectStyled.classList.remove('active');
+    selectNative.selectedIndex = parseInt(li.dataset.index!);
+    ul.style.display = 'none';
+    this.onDropdownSelect(li.dataset.value!);
+  }
+
+  private renderDropdown() {
+
+    // document.addEventListener("click", function (e) {
+    //   const ul = document.querySelector("ul.select-options") as HTMLElement;
+    //   const selectStyled = document.querySelector(".styled-select") as HTMLElement;
+
+    //   selectStyled.classList.remove('active');
+    //   ul.style.display = "none";
+    // });
+    return (<div className="select">
+      <select className="select-hidden" name="tabs_selector" value={!this.state.isShowMyTokensOnly ? this.props.selectedKey.asset : "manage"}>
+        {this.props.assets.map(asset => (<option value={asset}>{`${asset}-DAI`}</option>))}
+        <option value="manage">Manage</option>
+      </select>
+      <div className="styled-select" onClick={this.onStyledSelectClick}>
+        {!this.state.isShowMyTokensOnly ? this.props.selectedKey.asset + "-DAI" : "Manage"}
+      </div>
+      <ul className="select-options">
+        {this.props.assets.map((asset, i) => (<li data-value={asset} data-index={i} onClick={this.onLiClick.bind(this)}>{`${asset}-DAI`}</li>))}
+        <li data-value="manage" data-index={this.props.assets.length} onClick={this.onLiClick.bind(this)}>Manage</li>
+
+      </ul>
+    </div>);
   }
 
 }
