d46be3cfdb982340f0177db23f7590305dbb3eac
Rodion Kharabet <pensioneer4ik@gmail.com>
2020-02-19 12:18:57 +0200

    Update header link Lend/Trade → Lend

--- packages/torque/src/layout/HeaderOps.tsx
+++ packages/torque/src/layout/HeaderOps.tsx
@@ -48,7 +48,7 @@ export class HeaderOps extends Component<IHeaderOpsProps, IHeaderOpsState> {
           { id: 1, title: "Wallets", link: "/wallet/b", external: false },
           // { id: 2, title: "Borrow", link: "/borrow/w", external: false },
           { id: 3, title: "Dashboard", link: "/dashboard/w", external: false },
-          { id: 4, title: "Lend/Trade", link: "https://fulcrum.trade", external: true }
+          { id: 4, title: "Lend", link: "https://fulcrum.trade", external: true }
         ]
       };
     } else {
@@ -57,7 +57,7 @@ export class HeaderOps extends Component<IHeaderOpsProps, IHeaderOpsState> {
           { id: 1, title: "Wallets", link: "/wallet/b", external: false },
           // { id: 2, title: "Borrow", link: "/borrow/n", external: false },
           { id: 3, title: "Dashboard", link: "/dashboard/n", external: false },
-          { id: 4, title: "Lend/Trade", link: "https://fulcrum.trade", external: true }
+          { id: 4, title: "Lend", link: "https://fulcrum.trade", external: true }
         ]
       };
     }
