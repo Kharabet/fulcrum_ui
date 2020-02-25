aca8d298025be08f070194771dead6b000434f15
Rodion Kharabet <pensioneer4ik@gmail.com>
2020-02-25 19:00:13 +0200

    Revert "Disable ENS setup"
    This reverts commit 7052753175509e7c06e59fa79861ce32ca52144c.

--- packages/torque/src/pages/DashboardPage.tsx
+++ packages/torque/src/pages/DashboardPage.tsx
@@ -90,15 +90,13 @@ export class DashboardPage extends PureComponent<
     };
 
     let isENSSetup;
-    // if (this.state.walletDetails.walletType === WalletType.NonWeb3) {
-    //   if (walletAddress) {
-    //     isENSSetup = await TorqueProvider.Instance.checkENSSetup(walletAddress);
-    //   }
-    // } else {
-    //   isENSSetup = true;
-    // }
-    isENSSetup = undefined;
-
+    if (this.state.walletDetails.walletType === WalletType.NonWeb3) {
+      if (walletAddress) {
+        isENSSetup = await TorqueProvider.Instance.checkENSSetup(walletAddress);
+      }
+    } else {
+      isENSSetup = true;
+    }
 
     let items: IBorrowedFundsState[] = [];
     let itemsAwaiting: ReadonlyArray<BorrowRequestAwaiting> = [];
