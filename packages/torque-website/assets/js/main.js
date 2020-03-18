f0cb58aacb5776aaa314ff76c2fd0abc66b108d1
Diana Kovalyova <dianaborysivna@gmail.com>
2020-03-18 16:03:56 +0200

    Add js for accordion

--- packages/torque-website/assets/js/main.js
+++ packages/torque-website/assets/js/main.js
@@ -23,4 +23,35 @@ window.addEventListener('load', function () {
     }
 
     toggleSwitch.addEventListener('change', switchTheme, false);
+    
+    //accordion
+    var accordions = document.querySelectorAll('#accordion .accordion-toggle');
+    for (var i = 0; i < accordions.length; i++) {
+        accordions[i].addEventListener("click", updateAccordion, false);
+    }
+    
+    function setAriaAttr (el, ariaType, newProperty) {
+        el.setAttribute(ariaType, newProperty);
+    };
+    function setAccordionAria (el, expanded) {
+        switch (expanded) {
+            case "true":
+                setAriaAttr(el, 'aria-hidden', 'false');
+                break;
+            case "false":
+                setAriaAttr(el, 'aria-hidden', 'true');
+                break;
+            default:
+                break;
+        }
+    };
+    function updateAccordion(e) {   
+        if(e.currentTarget.parentElement.classList.contains("active")){        
+            setAccordionAria(e.currentTarget.nextElementSibling, "false");
+            e.currentTarget.parentElement.classList.remove("active");
+        }else{
+            setAccordionAria(e.currentTarget.nextElementSibling, "true");
+            e.currentTarget.parentElement.classList.add("active");
+        }
+    }
 });
