8b9bc6e8334caed7ccb280ef0ba2a040ae01ad72
Rodion Kharabet <pensioneer4ik@gmail.com>
2020-03-20 14:44:25 +0200

    move functions out of scope window onload

--- packages/torque-website/assets/js/main.js
+++ packages/torque-website/assets/js/main.js
@@ -1,12 +1,119 @@
 const apiUrl = "https://fulcrum-api-dev.herokuapp.com/api";
 
-async function getTorqueBorrowApr() {
-    const requestUrl = `${apiUrl}/torque-borrow-rate-apr`;
-    const respone = await fetch(requestUrl);
-    const responseJson = await response.json();
 
+window.addEventListener('load', function () {
+    //switch theme
+    var toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+    if (currentTheme === null) {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+        toggleSwitch.checked = false;
+    }
+    if (currentTheme)
+        if (currentTheme === 'dark')
+            toggleSwitch.checked = true;
+
+
+    toggleSwitch.addEventListener('change', switchTheme, false);
+
+    //accordion
+    var accordions = document.querySelectorAll('#accordion .accordion-toggle');
+    for (var i = 0; i < accordions.length; i++) {
+        accordions[i].addEventListener("click", updateAccordion, false);
+    }
+
+    function setAriaAttr(el, ariaType, newProperty) {
+        el.setAttribute(ariaType, newProperty);
+    };
+
+
+    var url = document.querySelectorAll('.nav-menu');
+    for (var i = 0; i < url.length; i++) {
+        if (url[i].getAttribute("href") == window.location.pathname)
+            url[i].classList.add("active-url");
+    }
+
+
+    const formLoan = document.querySelector('.form-loan');
+    formLoan.addEventListener("input", onLoanInputChange, false);
+
+
+    const itemForm = document.querySelectorAll('.item-form');
+    itemForm.forEach(item => {
+        item.querySelector('.select-styled').addEventListener("click", onStyledSelectClick);
+        item.querySelectorAll('.li-options').forEach(option => option.addEventListener("click", onItemFormLiClick));
+    });
+});
+
+window.onclick = function (event) {
+    const listSelect = document.querySelectorAll("ul.select-options");
+    const selectStyled = document.querySelectorAll(".select-styled");
+    for (let i = 0; i < listSelect.length; i++) {
+        if (event.target !== listSelect)
+            listSelect[i].style.display = "none";
+    }
+    selectStyled.forEach(item => item.classList.remove('active'));
+    if (event.target !== listSelect) {
+        document.body.classList.remove('open-modal');
+    }
 }
 
+
+async function collateralEstimate(loanAsset, collateralAsset, amount) {
+    const requestUrl = `${apiUrl}//borrow-deposit-estimate?borrow_asset=${loanAsset}&collateral_asset=${collateralAsset}&amount=${amount}`;
+    const collateralEstimateResponse = await fetch(requestUrl);
+    const responseJson = await collateralEstimateResponse.json();
+    const depositAmount = responseJson.depositAmount;
+    return depositAmount;
+}
+async function onLoanInputChange(e) {
+    const form = e.currentTarget;
+    const inputLoan = form.querySelector('.input-loan');
+    const inputCollateral = form.querySelector('.input-collateral');
+    const loanAsset = form.querySelector(".item-form.loan .select-styled").dataset.asset;
+    const collateralAsset = form.querySelector(".item-form.collateral .select-styled").dataset.asset;
+    const inputLoanValue = inputLoan.value;
+    inputLoanValue.includes("")
+        ? form.classList.remove('active')
+        : form.classList.add('active');
+    const collateralEstimateValue = await collateralEstimate(loanAsset, collateralAsset, inputLoanValue);
+    inputCollateral.value = collateralEstimateValue;
+}
+
+function updateAccordion(e) {
+    if (e.currentTarget.parentElement.classList.contains("active")) {
+        setAccordionAria(e.currentTarget.nextElementSibling, "false");
+        e.currentTarget.parentElement.classList.remove("active");
+    } else {
+        setAccordionAria(e.currentTarget.nextElementSibling, "true");
+        e.currentTarget.parentElement.classList.add("active");
+    }
+}
+
+function setAccordionAria(el, expanded) {
+    switch (expanded) {
+        case "true":
+            setAriaAttr(el, 'aria-hidden', 'false');
+            break;
+        case "false":
+            setAriaAttr(el, 'aria-hidden', 'true');
+            break;
+        default:
+            break;
+    }
+};
+
+function switchTheme(e) {
+
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+    }
+}
 function renderBorrowApr(apr) {
 
 }
@@ -31,7 +138,8 @@ function onStyledSelectClick(event) {
     }
     document.body.classList.add('open-modal');
 }
-function onItemFormLiClick (event) {
+
+function onItemFormLiClick(event) {
     event.stopPropagation();
     const li = event.currentTarget;
     const itemForm = li.closest('.item-form');
@@ -55,111 +163,10 @@ function onItemFormLiClick (event) {
     }
     document.body.classList.remove('open-modal');
 }
-window.addEventListener('load', function () {
-    //switch theme
-    var toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
-    if (currentTheme === null) {
-        document.documentElement.setAttribute('data-theme', 'light');
-        localStorage.setItem('theme', 'light');
-        toggleSwitch.checked = false;
-    }
-    if (currentTheme)
-        if (currentTheme === 'dark')
-            toggleSwitch.checked = true;
 
-    function switchTheme(e) {
+async function getTorqueBorrowApr() {
+    const requestUrl = `${apiUrl}/torque-borrow-rate-apr`;
+    const respone = await fetch(requestUrl);
+    const responseJson = await response.json();
 
-        if (e.target.checked) {
-            document.documentElement.setAttribute('data-theme', 'dark');
-            localStorage.setItem('theme', 'dark');
-        }
-        else {
-            document.documentElement.setAttribute('data-theme', 'light');
-            localStorage.setItem('theme', 'light');
-        }
-    }
-
-    toggleSwitch.addEventListener('change', switchTheme, false);
-
-    //accordion
-    var accordions = document.querySelectorAll('#accordion .accordion-toggle');
-    for (var i = 0; i < accordions.length; i++) {
-        accordions[i].addEventListener("click", updateAccordion, false);
-    }
-
-    function setAriaAttr(el, ariaType, newProperty) {
-        el.setAttribute(ariaType, newProperty);
-    };
-    function setAccordionAria(el, expanded) {
-        switch (expanded) {
-            case "true":
-                setAriaAttr(el, 'aria-hidden', 'false');
-                break;
-            case "false":
-                setAriaAttr(el, 'aria-hidden', 'true');
-                break;
-            default:
-                break;
-        }
-    };
-    function updateAccordion(e) {
-        if (e.currentTarget.parentElement.classList.contains("active")) {
-            setAccordionAria(e.currentTarget.nextElementSibling, "false");
-            e.currentTarget.parentElement.classList.remove("active");
-        } else {
-            setAccordionAria(e.currentTarget.nextElementSibling, "true");
-            e.currentTarget.parentElement.classList.add("active");
-        }
-    }
-
-    var url = document.querySelectorAll('.nav-menu');
-    for (var i = 0; i < url.length; i++) {
-        if (url[i].getAttribute("href") == window.location.pathname)
-            url[i].classList.add("active-url");
-    }
-
-    async function onLoanInputChange(e) {
-        const form = e.currentTarget;
-        const inputLoan = form.querySelector('.input-loan');
-        const inputCollateral = form.querySelector('.input-collateral');
-        const loanAsset = form.querySelector(".item-form.loan .select-styled").dataset.asset;
-        const collateralAsset = form.querySelector(".item-form.collateral .select-styled").dataset.asset;
-        const inputLoanValue = inputLoan.value;
-        inputLoanValue.includes("")
-            ? form.classList.remove('active')
-            : form.classList.add('active');
-        const collateralEstimateValue = await collateralEstimate(loanAsset, collateralAsset, inputLoanValue);
-        inputCollateral.value = collateralEstimateValue;
-    }
-
-    async function collateralEstimate(loanAsset, collateralAsset, amount) {
-        const requestUrl = `${apiUrl}//borrow-deposit-estimate?borrow_asset=${loanAsset}&collateral_asset=${collateralAsset}&amount=${amount}`;
-        const collateralEstimateResponse = await fetch(requestUrl);
-        const responseJson = await collateralEstimateResponse.json();
-        const depositAmount = responseJson.depositAmount;
-        return depositAmount;
-    }
-
-    const formLoan = document.querySelector('.form-loan');
-    formLoan.addEventListener("input", onLoanInputChange, false);
-
-
-    const itemForm = document.querySelectorAll('.item-form');
-    itemForm.forEach(item => {
-        item.querySelector('.select-styled').addEventListener("click", onStyledSelectClick);
-        item.querySelectorAll('.li-options').forEach(option => option.addEventListener("click", onItemFormLiClick));
-    });
-});
-
-window.onclick = function (event) {
-    const listSelect = document.querySelectorAll("ul.select-options");
-    const selectStyled = document.querySelectorAll(".select-styled");
-    for (let i = 0; i < listSelect.length; i++) {
-        if (event.target !== listSelect)
-            listSelect[i].style.display = "none";
-    }
-    selectStyled.forEach(item => item.classList.remove('active'));
-    if (event.target !== listSelect) {
-        document.body.classList.remove('open-modal');
-    }
 }
