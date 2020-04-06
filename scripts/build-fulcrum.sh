9f59676b6ecd02ef55547e30ef99f0c14084d3d5
Rodion Kharabet <pensioneer4ik@gmail.com>
2020-04-06 21:25:34 +0300

    Remove web3-react build from netlify workflow. Add web3-react dist folders to the repo.

--- scripts/build-fulcrum.sh
+++ scripts/build-fulcrum.sh
@@ -3,9 +3,6 @@
 chmod a+x ./scripts/build-alchemy-web3.sh
 ./scripts/build-alchemy-web3.sh
 
-chmod a+x ./scripts/build-web3-react.sh
-./scripts/build-web3-react.sh
-
 cd ./packages/fulcrum
 yarn run build
 cd ./../..
