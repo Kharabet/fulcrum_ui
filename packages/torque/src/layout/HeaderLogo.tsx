0a76494798480baa24c3f87865c53c7db4fae0b8
Rodion Kharabet <pensioneer4ik@gmail.com>
2020-04-10 12:26:53 +0300

    Update responsive torque header

--- packages/torque/src/layout/HeaderLogo.tsx
+++ packages/torque/src/layout/HeaderLogo.tsx
@@ -1,14 +1,20 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
-import { ReactComponent as TorqueLogo } from "../assets/images/torque_logo.svg";
+import {ReactComponent as TorqueLogo} from "../assets/images/torque_logo.svg";
+import {ReactComponent as TorqueLogoPartial} from "../assets/images/torque_logo_partial.svg";
 
 export class HeaderLogo extends Component {
   public render() {
     return (
       <div className="header-logo">
         <Link to="/">
-          <TorqueLogo />
+          <div className="header-logo-full">
+            <TorqueLogo />
+          </div>
+          <div className="header-logo-partial">
+            <TorqueLogoPartial />
+          </div>
         </Link>
       </div>
     );
