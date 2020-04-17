97d3da75f9e60351ca81c0f9bbb7f480d028e6da
Diana Kovalyova <dianaborysivna@gmail.com>
2020-04-17 12:46:33 +0300

    Clear tokentradeGrid/innerOwnTokenGrid

--- packages/fulcrum/src/components/TradeTokenGrid.tsx
+++ packages/fulcrum/src/components/TradeTokenGrid.tsx
@@ -1,10 +1,6 @@
 import React, { Component } from "react";
 import { Asset } from "../domain/Asset";
 import { PositionType } from "../domain/PositionType";
-import { FulcrumProviderEvents } from "../services/events/FulcrumProviderEvents";
-import { ProviderChangedEvent } from "../services/events/ProviderChangedEvent";
-import { TradeTransactionMinedEvent } from "../services/events/TradeTransactionMinedEvent";
-import { FulcrumProvider } from "../services/FulcrumProvider";
 import { TradeTokenGridHeader } from "./TradeTokenGridHeader";
 import { ITradeTokenGridRowProps, TradeTokenGridRow } from "./TradeTokenGridRow";
 import { TradeTokenCardMobile } from "./TradeTokenCardMobile";
@@ -26,39 +22,16 @@ export class TradeTokenGrid extends Component<ITradeTokenGridProps, ITradeTokenG
 
   constructor(props: ITradeTokenGridProps) {
     super(props);
-
     this._isMounted = false;
     this.state = {
       positionType: PositionType.LONG
     }
-    FulcrumProvider.Instance.eventEmitter.on(FulcrumProviderEvents.ProviderChanged, this.onProviderChanged);
-    FulcrumProvider.Instance.eventEmitter.on(FulcrumProviderEvents.TradeTransactionMined, this.onTradeTransactionMined);
   }
 
   private _isMounted: boolean;
 
-  public async derivedUpdate() {
-  }
-
-  public componentWillUnmount(): void {
-    this._isMounted = false;
-
-    FulcrumProvider.Instance.eventEmitter.removeListener(FulcrumProviderEvents.ProviderChanged, this.onProviderChanged);
-    FulcrumProvider.Instance.eventEmitter.removeListener(FulcrumProviderEvents.TradeTransactionMined, this.onTradeTransactionMined);
-  }
-  public componentWillMount(): void {
-    this.derivedUpdate();
-  }
-
   public componentDidMount(): void {
     this._isMounted = true;
-    this.derivedUpdate();
-  }
-
-  public componentDidUpdate(prevProps: Readonly<ITradeTokenGridProps>, prevState: Readonly<ITradeTokenGridState>, snapshot?: any): void {
-    if (this.props.selectedTabAsset !== prevProps.selectedTabAsset) {
-      this.derivedUpdate();
-    }
   }
 
   public render() {
@@ -112,13 +85,6 @@ export class TradeTokenGrid extends Component<ITradeTokenGridProps, ITradeTokenG
       </div>
     );
   }
-  private onProviderChanged = async (event: ProviderChangedEvent) => {
-    await this.derivedUpdate();
-  };
-
-  private onTradeTransactionMined = async (event: TradeTransactionMinedEvent) => {
-    await this.derivedUpdate();
-  };
 
   private changeGridPositionType= async (positionType: PositionType) =>{
     await this._isMounted && this.setState({...this.state, positionType: positionType})
