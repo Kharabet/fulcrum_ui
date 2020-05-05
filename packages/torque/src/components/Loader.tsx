92e220f06c0c8f80978a14bdb1844f20bbb4f8f7
Diana Kovalyova <dianaborysivna@gmail.com>
2020-05-05 14:40:07 +0300

    Add key for loader

--- packages/torque/src/components/Loader.tsx
+++ packages/torque/src/components/Loader.tsx
@@ -15,7 +15,7 @@ export class Loader extends Component<ILoaderProps> {
     const animationDuration = 0.7 * this.props.quantityDots;
     let wrapperDots = [];
     for (var i = 0; i < this.props.quantityDots; i++) {
-      wrapperDots.push(<span className={`${this.props.sizeDots}-dots`} style={{animationDuration : `${animationDuration}s`}}></span>)
+      wrapperDots.push(<span key={i} className={`${this.props.sizeDots}-dots`} style={{animationDuration : `${animationDuration}s`}}></span>)
     }
     return wrapperDots;
   }
