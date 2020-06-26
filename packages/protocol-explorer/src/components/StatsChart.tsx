d32094cb2790f90f9f827155ba8536f3808acee0
Diana Kovalyova <dianaborysivna@gmail.com>
2020-06-26 18:15:10 +0300

    Add custom tooltip for stats page

--- packages/protocol-explorer/src/components/StatsChart.tsx
+++ packages/protocol-explorer/src/components/StatsChart.tsx
@@ -15,6 +15,7 @@ interface IStatsChartState {
   utilization: Array<number>;
   apr: Array<number>;
   tvl: Array<number>;
+  aprChange24: Array<number>;
 }
 
 export class StatsChart extends Component<IStatsChartProps, IStatsChartState> {
@@ -28,7 +29,8 @@ export class StatsChart extends Component<IStatsChartProps, IStatsChartState> {
       labels: [],
       tvl: [],
       apr: [],
-      utilization: []
+      utilization: [],
+      aprChange24: [],
     };
     this.assetsShown = [
       Asset.ETH,
@@ -74,6 +76,7 @@ export class StatsChart extends Component<IStatsChartProps, IStatsChartState> {
     let tvl: any = [];
     let apr: any = [];
     let utilization: any = [];
+    let aprChange24: any = [];
     const period = this.state.periodChart;
     if (responseJson.success) {
       responseJson.data.forEach(function (item: any) {
@@ -84,11 +87,12 @@ export class StatsChart extends Component<IStatsChartProps, IStatsChartState> {
         tvl.push(+item["tvl"]);
         apr.push(+item["supplyInterestRate"]);
         utilization.push(+item["utilization"]);
+        aprChange24.push(+item["tvlChange24h"]);        
       });
     } else {
       console.error(responseJson.message)
     }
-    await this.setState({ ...this.state, tvl: tvl, apr: apr, utilization: utilization, labels: labels })
+    await this.setState({ ...this.state, tvl: tvl, apr: apr, utilization: utilization, labels: labels, aprChange24: aprChange24 })
   }
 
   public render() {
@@ -99,6 +103,7 @@ export class StatsChart extends Component<IStatsChartProps, IStatsChartState> {
 
       return {
         labels: this.state.labels,
+        aprChange24: this.state.aprChange24,
         datasets: [{
           label: 'TVL',
           yAxisID: 'A',
@@ -106,7 +111,7 @@ export class StatsChart extends Component<IStatsChartProps, IStatsChartState> {
           backgroundColor: "transparent",
           pointBackgroundColor: '#276BFB',
           borderColor: '#276BFB',
-          borderWidth: 2,
+          borderWidth: 2
         },
         {
           label: 'Supply APR, %',
@@ -115,6 +120,7 @@ export class StatsChart extends Component<IStatsChartProps, IStatsChartState> {
           backgroundColor: "transparent",
           pointBackgroundColor: '#33DFCC',
           borderColor: '#33DFCC',
+          borderWidth: 2
         },
         {
           label: 'Utilization, %',
@@ -123,6 +129,7 @@ export class StatsChart extends Component<IStatsChartProps, IStatsChartState> {
           backgroundColor: "transparent",
           pointBackgroundColor: '#B79EFF',
           borderColor: '#B79EFF',
+          borderWidth: 2
         }]
       }
     }
@@ -168,35 +175,23 @@ export class StatsChart extends Component<IStatsChartProps, IStatsChartState> {
       },
       layout: {
         padding: {
+          top: 10,
+          bottom: 10
         }
       },
       tooltips: {
-        backgroundColor: 'rgba(0, 0, 0, 0)',
-        displayColors: false,
-        bodyFontFamily: 'Muli',
-        bodyFontSize: 30,
-        bodyFontColor: '#283049',
-        bodyFontStyle: 'bold',
-        titleFontFamily: 'Muli',
-        titleFontSize: 14,
-        titleFontColor: '#8992A4',
-        titleFontStyle: 'black',
-        titleMarginBottom: 10,
-        mode: 'nearest',
-        position: 'nearest',
-        intersect: true,
+        enabled: false,
+        mode: 'point',
+        custom: this.customTooltips,
         callbacks: {
           label: function (tooltipItems: any, data: any) {
-            var label = data.datasets[tooltipItems.datasetIndex].label || '';
-            var number;
-            if (tooltipItems.yLabel > 1000000) {
-              number = `${(tooltipItems.yLabel / 1000000).toFixed(3)}m`;
-            } else if (tooltipItems.yLabel > 1000) {
-              number = `${(tooltipItems.yLabel / 1000).toFixed(3)}k`;
-            } else {
-              number = `${(tooltipItems.yLabel).toFixed(3)} `;
-            }
-            return (label === 'TVL') ? `$${number}` : `${number}%`;
+            const change24 = data.aprChange24[tooltipItems.index];
+            const currency = data.datasets[tooltipItems.datasetIndex].label === "TVL" ? true : false;
+            if (tooltipItems.yLabel > 100000)
+              return { data: `${(tooltipItems.yLabel / 1000000).toFixed(3)}m`, change24: change24, currency: currency };
+            if (tooltipItems.yLabel > 100)
+              return { data: `${(tooltipItems.yLabel / 1000).toFixed(3)}k`, change24: change24, currency: currency };
+            return { data: `${tooltipItems.yLabel.toFixed(3)}`, change24: change24, currency: currency };
           }
         }
       },
@@ -220,12 +215,57 @@ export class StatsChart extends Component<IStatsChartProps, IStatsChartState> {
           </div>
           <GroupButton setPeriodChart={this.setPeriodChart} />
         </div>
-        <Line ref="chart" data={chartData} options={options} height={70} />
+        <div className="wrapper-chart">
+          <div id="chartjs">
+            <Line ref="chart" data={chartData} options={options} height={80} />
+          </div>
+          <div id="chartjs-tooltip"><table></table></div>
+        </div>
       </React.Fragment>
     );
   }
 
   public setPeriodChart = (period: number) => {
     this.setState({ ...this.state, periodChart: period })
+  }
+
+  public customTooltips = (tooltip: any) => {
+    let tooltipEl = document.getElementById('chartjs-tooltip');
+    let chartEl = document.getElementById('chartjs');
+    if (!tooltipEl) {
+      tooltipEl = document.createElement('div');
+      tooltipEl.id = 'chartjs-tooltip';
+      tooltipEl.innerHTML = "<div></div>"
+      document.body.appendChild(tooltipEl);
+    }
+    const heighttooltipEl = tooltipEl.offsetHeight;
+    const widthTooltipEl = tooltipEl.offsetWidth;
+    const widthChart = chartEl!.offsetWidth;
+    if (tooltip.opacity === 0) {
+      tooltipEl.style.opacity = '0';
+      tooltipEl.style.left = -widthTooltipEl + 'px';
+      return;
+    }
+    function getBody(bodyItem: any) {
+      return bodyItem.lines[0];
+    }
+    if (tooltip.body) {
+      const titleLines = tooltip.title || [];
+      const dataLines = tooltip.body.map(getBody);
+      let innerHtml = `<tbody class="${heighttooltipEl + 55 > tooltip.caretY ? `bottom` : ``} ${widthChart - tooltip.caretX < widthTooltipEl ? `right` : `left`}">`;
+      titleLines.forEach(function (title: number) {
+        innerHtml += `<tr class="chartjs-tooltip-time"><th><span>${title}</span></th></tr>`;
+      });
+      dataLines.forEach(function (body: any, index: number) {
+        innerHtml += `<tr  class="chartjs-tooltip-value"><td><span>${body.currency ? `<span class="sign sign-currency">$</span>` : ``}${body.data}</span>${!body.currency ? `<span class="sign sign-currency">%</span>` : ``}</td></tr><tr class="chartjs-tooltip-change24 ${body.change24 < 0 ? `down` : `up`} ${heighttooltipEl + 55 < tooltip.caretY ? `bottom` : `top`} ${widthChart - tooltip.caretX < widthTooltipEl ? `right` : `left`}"><td><span>${Math.abs(body.change24).toFixed(4)}%</span></td></tr>`;
+      });
+      innerHtml += `</tbody>`;
+      const tableRoot = tooltipEl.querySelector('table') as HTMLElement;
+      tableRoot.innerHTML = innerHtml;
+    }
+    tooltipEl.style.opacity = '1';
+    tooltipEl.style.position = 'absolute';
+    tooltipEl.style.left = (widthChart - tooltip.caretX < widthTooltipEl) ? tooltip.caretX - widthTooltipEl + 5 + 'px' : tooltip.caretX - 7 + 'px';
+    tooltipEl.style.top = (heighttooltipEl + 55 < tooltip.caretY) ? tooltip.caretY - heighttooltipEl - 55 + 'px' : tooltip.caretY + 55 + 'px';
   }
 }
