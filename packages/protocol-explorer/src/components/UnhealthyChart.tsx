00f3796af1ce1725fcd59981370a37a493fef0f8
Diana Kovalyova <dianaborysivna@gmail.com>
2020-06-30 17:51:20 +0300

    Add custom tooltip for unhealthy chart

--- packages/protocol-explorer/src/components/UnhealthyChart.tsx
+++ packages/protocol-explorer/src/components/UnhealthyChart.tsx
@@ -21,6 +21,38 @@ export const UnhealthyChart = (props: IUnhealthyChartProps) => {
   }
   const canvas = document.createElement('canvas');
   const chartData = getData(canvas);
+
+  const customTooltips = (tooltip: any) => {
+    let tooltipEl = document.getElementById('chartjs-doughnut-tooltip');
+    if (!tooltipEl) {
+      tooltipEl = document.createElement('div');
+      tooltipEl.id = 'chartjs-bar-tooltip';
+      tooltipEl.innerHTML = "<div></div>"
+      document.body.appendChild(tooltipEl);
+    }
+    if (tooltip.opacity === 0) {
+      tooltipEl.style.opacity = '0';
+      tooltipEl.style.left = -tooltip.width + 'px';
+      return;
+    }
+    function getBody(bodyItem: any) {
+      return bodyItem.lines[0];
+    }
+    if (tooltip.body) {
+      const bodyLines = tooltip.body.map(getBody);
+      let innerHtml = `<tbody style="padding: 10px">`;
+      bodyLines.forEach(function (body: any) {
+        innerHtml += `<tr><td>The size of your trade will move the price by <span class="value">${body}%</span>. If you wish to guarantee a price, edit the exit price to create a limit sell order.</td></tr>`;
+      });
+      innerHtml += '</tbody>';
+      const tableRoot = tooltipEl.querySelector('table') as HTMLElement;
+      tableRoot.innerHTML = innerHtml;
+    }
+    tooltipEl.style.opacity = '1';
+    tooltipEl.style.position = 'absolute';
+    tooltipEl.style.left = tooltip.caretX + 'px';
+    tooltipEl.style.top = tooltip.caretY - 2 * tooltip.height + 'px';
+  }
   const options = {
     cutoutPercentage: 65,
     rotation: 0,
@@ -38,12 +70,22 @@ export const UnhealthyChart = (props: IUnhealthyChartProps) => {
     },
     legend: {
       display: false
+    },
+    tooltips: {
+      enabled: false,
+      custom: customTooltips,
+      callbacks: {
+        label: function (tooltipItems: any, data: any) {
+          return data.datasets[tooltipItems.datasetIndex].data[tooltipItems.index];
+        }
+      }
     }
   }
   return (
     <React.Fragment>
       <section className="wrapper-chartjs-doughnut">
         <Doughnut data={chartData} options={options} height={200} />
+        <div id="chartjs-doughnut-tooltip"><table></table></div>
       </section>
     </React.Fragment>
   );
