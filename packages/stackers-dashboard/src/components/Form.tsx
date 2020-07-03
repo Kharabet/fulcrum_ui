31fe30cabdca5f42bca1706faee886684b5ac6f5
Diana Kovalyova <dianaborysivna@gmail.com>
2020-07-03 15:08:01 +0300

    Add responsive style for dashboard page

--- packages/stackers-dashboard/src/components/Form.tsx
+++ packages/stackers-dashboard/src/components/Form.tsx
@@ -9,27 +9,31 @@ export class Form extends Component {
   public render() {
     return (
       <React.Fragment>
-        <div className="calculator">
-          <div className="calculator-row">
-            <div>
-              <div className="row-header">My BZRX balance:</div>
-              <div className="row-body">
-                  100,000,000
-                  <span><CircleBzx /></span>
+        <div className="container">
+          <div className="calculator">
+            <div className="calculator-row">
+              <div>
+                <div className="row-header">My BZRX balance:</div>
+                <div className="row-body">
+                  <span className="value">
+                    100,000,000
+                   <span className="icon"><CircleBzx /></span>
+                  </span>
+                </div>
+                <div className="row-footer">BZRX</div>
               </div>
-              <div className="row-footer">BZRX</div>
+              <div className="reward-item">
+                <div className="row-header">My rewards balance:</div>
+                <div className="row-body">0,14</div>
+                <div className="row-footer">bZxDAO-Balancer tokens</div>
+              </div>
             </div>
-            <div className="reward-item">
-              <div className="row-header">My rewards balance:</div>
-              <div className="row-body">0,14</div>
-              <div className="row-footer">bZxDAO-Balancer tokens</div>
+            <div className="group-buttons">
+              <a href="/" className="button">Stake</a>
+              <a href="/" className="button">Unstake</a>
+              <a href="/" className="button">Claim Rewards</a>
+              <a href="/" className="button">Explore Reward Pool</a>
             </div>
-          </div>
-          <div className="group-buttons">
-            <a href="/" className="button">Stake</a>
-            <a href="/" className="button">Unstake</a>
-            <a href="/" className="button">Claim Rewards</a>
-            <a href="/" className="button">Explore Reward Pool</a>
           </div>
         </div>
       </React.Fragment>
