0965606b5c36d796e238debe1a4eef380c005015
OlehX <vovnyankoolehx@gmail.com>
2020-07-08 18:22:25 +0300

    Add pagination to unhealthy loans

--- packages/protocol-explorer/src/components/LoanGrid.tsx
+++ packages/protocol-explorer/src/components/LoanGrid.tsx
@@ -1,21 +1,53 @@
 import React, { Component } from "react";
 import { LoanRow, ILoanRowProps } from "./LoanRow";
+import { ReactComponent as ArrowPagination } from "../assets/images/icon_pagination.svg";
+import { runInThisContext } from "vm";
+
 interface ILoanGridProps {
   events: ILoanRowProps[]
 }
 
 interface ILoanGridState {
+
+  numberPagination: number;
+  quantityGrids: number;
+  isLastRow: boolean;
 }
 
 export class LoanGrid extends Component<ILoanGridProps, ILoanGridState> {
+  private quantityVisibleRow = 25;
+  private assetItems = this.props.events.map((e: ILoanRowProps, i: number) => <LoanRow key={i} {...e} />);
+
+
   constructor(props: any) {
     super(props);
     this.state = {
+      numberPagination: 0,
+      quantityGrids: 0,
+      isLastRow: false
     };
+
+    // let i = 0;
+    // while (i < 25) {
+    //   const assetItem = <LoanRow key={i + 10} {...this.props.events[0]} />;
+    //   this.assetItems.push(assetItem)
+    //   i++;
+    // }
+
+  }
+
+  public UNSAFE_componentWillMount(): void {
+    const quantityGrids = Math.floor(this.assetItems.length / this.quantityVisibleRow);
+    const isLastRow = this.assetItems.length === (this.state.numberPagination + 1) * this.quantityVisibleRow;
+    this.setState({ ...this.state, quantityGrids: quantityGrids, isLastRow: isLastRow })
   }
 
   public render() {
-    const assetItems = this.props.events.map((e: ILoanRowProps, i: number) => <LoanRow key={i} {...e} />);
+
+    const assetItems = this.assetItems.slice(this.quantityVisibleRow * this.state.numberPagination, this.quantityVisibleRow * this.state.numberPagination + this.quantityVisibleRow);
+    // const assetItems = this.props.events.slice(this.quantityVisibleRow * this.state.numberPagination, this.quantityVisibleRow * this.state.numberPagination + this.quantityVisibleRow).map((e, i) => <LoanRow key={i} {...e} />);
+    if (assetItems.length === 0) return null;
+
     return (
       <React.Fragment>
         <div className="table table-loan">
@@ -26,8 +58,27 @@ export class LoanGrid extends Component<ILoanGridProps, ILoanGridState> {
             <div className="table-header-loan__action">Action</div>
           </div>
           {assetItems}
+          <div className="pagination">
+            <div className={`prev ${this.state.numberPagination === 0 ? `disabled` : ``}`} onClick={this.prevPagination}><ArrowPagination /></div>
+            <div className={`next ${this.state.numberPagination === this.state.quantityGrids || this.state.isLastRow ? `disabled` : ``}`} onClick={this.nextPagination}><ArrowPagination /></div>
+          </div>
         </div>
       </React.Fragment>
     );
+  }
+
+
+  public nextPagination = () => {
+
+    if (this.state.numberPagination !== this.state.quantityGrids && !this.state.isLastRow) {
+      const isLastRow = this.assetItems.length === (this.state.numberPagination + 2) * this.quantityVisibleRow;
+      this.setState({ ...this.state, numberPagination: this.state.numberPagination + 1, isLastRow: isLastRow });
+    }
+  }
+
+  public prevPagination = () => {
+    if (this.state.numberPagination !== 0) {
+      this.setState({ ...this.state, numberPagination: this.state.numberPagination - 1, isLastRow: false });
+    }
   }
 }
