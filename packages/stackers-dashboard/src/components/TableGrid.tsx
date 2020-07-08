b840f7e83f9fbceac236431f0a78c1a7373aff33
Diana Kovalyova <dianaborysivna@gmail.com>
2020-07-08 15:11:39 +0300

    Add scroll for responsive table

--- packages/stackers-dashboard/src/components/TableGrid.tsx
+++ packages/stackers-dashboard/src/components/TableGrid.tsx
@@ -13,17 +13,19 @@ export class TableGrid extends Component<ITableGridProps> {
   public render() {
     return (
       <React.Fragment>
-        <div className="grid">
-          <div className="grid-header">
-            <div className="tx">Tnx</div>
-            <div className="date">Date</div>
-            <div className="action">Action</div>
-            <div className="currency">Currency</div>
-            <div className="amount">Amount</div>
+        <div className="grid-wrapper">
+          <div className="grid">
+            <div className="grid-header">
+              <div className="tx">Tnx</div>
+              <div className="date">Date</div>
+              <div className="action">Action</div>
+              <div className="currency">Currency</div>
+              <div className="amount">Amount</div>
+            </div>
+            <div className="grid-body">
+              <TableRow isMobileMedia={this.props.isMobileMedia} />
+            </div>
           </div>
-          <div className="grid-body">
-            <TableRow isMobileMedia={this.props.isMobileMedia} />
-        </div>
         </div>
       </React.Fragment>
     );
