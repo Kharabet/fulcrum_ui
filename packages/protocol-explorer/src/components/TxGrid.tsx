f6744c9a546d22699b02c340adc65eb6ca0ed519
OlehX <vovnyankoolehx@gmail.com>
2020-07-08 18:55:48 +0300

    Show last 25 Tx

--- packages/protocol-explorer/src/components/TxGrid.tsx
+++ packages/protocol-explorer/src/components/TxGrid.tsx
@@ -10,6 +10,8 @@ interface ITxGridState {
 }
 
 export class TxGrid extends Component<ITxGridProps, ITxGridState> {
+  private lastEvents = this.props.events.filter((e: ITxRowProps, i: number) => i < 25);
+
   constructor(props: any) {
     super(props);
     this.state = {
@@ -18,12 +20,12 @@ export class TxGrid extends Component<ITxGridProps, ITxGridState> {
   }
 
   public render() {
-    const assetItems = this.props.events
+    const assetItems = this.lastEvents
       .sort((a, b) => { return this.state.typeSort === 'up' ? b.age.getTime() - a.age.getTime() : a.age.getTime() - b.age.getTime() })
       .map((e: ITxRowProps, i: number) => <TxRow key={i} {...e} />);
     return (
       <React.Fragment>
-        {this.props.events.length !== 0 &&
+        {this.lastEvents.length !== 0 &&
           <div className="table table-tx">
             <div className="table-header table-header-tx">
               <div className="table-header-tx__hash">Txn Hash</div>
