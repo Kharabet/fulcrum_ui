58b2f5076766334d1b6a70b17ce44b182f3a4276
Rodion Kharabet <pensioneer4ik@gmail.com>
2020-07-08 16:30:31 +0300

    Add address for bzrx token

--- packages/stackers-dashboard/src/services/ContractsSource.ts
+++ packages/stackers-dashboard/src/services/ContractsSource.ts
@@ -1,5 +1,5 @@
 import * as _ from "lodash";
-
+import { convertContract } from "../contracts/convert";
 
 const ethNetwork = process.env.REACT_APP_ETH_NETWORK;
 
@@ -8,7 +8,7 @@ export class ContractsSource {
 
   private static isInit = false;
 
-  private erc20Json: any;
+  private static convertJson: any;
   public networkId: number;
   public canWrite: boolean;
   public saiToDAIBridgeJson: any;
@@ -20,10 +20,82 @@ export class ContractsSource {
   }
 
   public async Init() {
-    if (ContractsSource.isInit && this.erc20Json) {
+    if (ContractsSource.isInit) {
       return;
     }
-    const network = ethNetwork || "1";
+    ContractsSource.convertJson = await import(`./../assets/artifacts/${ethNetwork}/convert.json`);
+
     ContractsSource.isInit = true;
   }
+
+  
+  public getBzrxV1Address(): string {
+    let address: string = "";
+    switch (this.networkId) {
+      case 1:
+        address = "";
+        break;
+      case 3:
+        address = "";
+        break;
+      case 4:
+        address = "";
+        break;
+      case 42:
+        address = "0xe3e682A8Fc7EFec410E4099cc09EfCC0743C634a";
+        break;
+    }
+    return address;
+  }
+  
+  public getBzrxAddress(): string {
+    let address: string = "";
+    switch (this.networkId) {
+      case 1:
+        address = "";
+        break;
+      case 3:
+        address = "";
+        break;
+      case 4:
+        address = "";
+        break;
+      case 42:
+        address = "0xB54Fc2F2ea17d798Ad5C7Aba2491055BCeb7C6b2";
+        break;
+    }
+    return address;
+  }
+  
+  public getConvertAddress(): string {
+    let address: string = "";
+    switch (this.networkId) {
+      case 1:
+        address = "";
+        break;
+      case 3:
+        address = "";
+        break;
+      case 4:
+        address = "";
+        break;
+      case 42:
+        address = "0xdff6eFB05Ee1ed73Ec89030c59Dcc035Aea092b9";
+        break;
+    }
+    return address;
+  }
+
+  
+  private async getConvertContractRaw(): Promise<convertContract> {
+    await this.Init();
+    return new convertContract(
+      ContractsSource.convertJson.abi,
+      this.getConvertAddress().toLowerCase(),
+      this.provider
+    );
+  }
+
+  public getConvertContract = _.memoize(this.getConvertContractRaw);
+
 }
