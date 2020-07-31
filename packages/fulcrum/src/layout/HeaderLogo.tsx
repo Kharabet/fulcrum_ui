3baaacdf40eee1b63c47607cdbe30200c312cc25
Diana Kovalyova <dianaborysivna@gmail.com>
2020-07-31 18:27:44 +0300

    Replace logo

--- packages/fulcrum/src/layout/HeaderLogo.tsx
+++ packages/fulcrum/src/layout/HeaderLogo.tsx
@@ -1,19 +1,19 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
-import {ReactComponent as FulcrumLogo} from "../assets/images/fulcrum_logo.svg";
-import {ReactComponent as FulcrumLogoPartial} from "../assets/images/fulcrum_logo_partial.svg";
+import { ReactComponent as FulcrumLogo } from "../assets/images/fulcrum_logo.svg";
+import { ReactComponent as FulcrumLogoSign } from "../assets/images/fulcrum_logo_partial.svg";
 
 export class HeaderLogo extends Component {
   public render() {
     return (
       <div className="header-logo">
         <Link to="/">
-          <div className="header-logo-full">
-            <FulcrumLogo />
+          <div className="fulcrum-logo-sign">
+            <FulcrumLogoSign />
           </div>
-          <div className="header-logo-partial">
-            <FulcrumLogoPartial />
+          <div className="fulcrum-logo">
+            <FulcrumLogo />
           </div>
         </Link>
       </div>
