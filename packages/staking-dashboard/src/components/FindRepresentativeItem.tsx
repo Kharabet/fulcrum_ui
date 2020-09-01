b4029b18840a019313cdbb4759bcac88d36e7a37
Rodion Kharabet <pensioneer4ik@gmail.com>
2020-09-01 06:03:39 +0300

    Staking: add titles on hover for Find reps modal

--- packages/staking-dashboard/src/components/FindRepresentativeItem.tsx
+++ packages/staking-dashboard/src/components/FindRepresentativeItem.tsx
@@ -16,33 +16,33 @@ const networkName = process.env.REACT_APP_ETH_NETWORK;
 
 export const FindRepresentativeItem = (props: IFindRepresentativeItemProps) => {
 
-  const bzrxAmount = props.representative.BZRX.div(10 ** 18).toFixed(2);
-  const vbzrxAmount = props.representative.vBZRX.div(10 ** 18).toFixed(2);
+  const bzrxAmount = props.representative.BZRX.div(10 ** 18);
+  const vbzrxAmount = props.representative.vBZRX.div(10 ** 18);
   //TODO: remove networkName
   const bptAmount = networkName === "kovan"
-    ? props.representative.LPToken.div(10 ** 6).toFixed(2)
-    : props.representative.LPToken.div(10 ** 18).toFixed(2);
+    ? props.representative.LPToken.div(10 ** 6)
+    : props.representative.LPToken.div(10 ** 18);
 
   return (
     <li className="item-find-representative" onClick={() => props.onRepClick()}>
       <img className="photo" src={props.representative.imageSrc} alt={`Representative ${props.representative.index}`} />
       <div className="name">{props.representative.name}</div>
       {props.representative.BZRX.gt(0) &&
-        <div className="token">
+        <div className="token" title={bzrxAmount.toFixed(18)}>
           <BzrxIcon />
-          <span>{bzrxAmount}</span>
+          <span>{bzrxAmount.toFixed(2)}</span>
         </div>
       }
       {props.representative.vBZRX.gt(0) &&
-        <div className="token">
+        <div className="token" title={vbzrxAmount.toFixed(18)}>
           <VBzrxIcon />
-          <span>{vbzrxAmount}</span>
+          <span>{vbzrxAmount.toFixed(2)}</span>
         </div>
       }
       {props.representative.LPToken.gt(0) &&
-        <div className="token">
+        <div className="token" title={bptAmount.toFixed(18)}>
           <BPTIcon />
-          <span>{bptAmount}</span>
+          <span>{bptAmount.toFixed(2)}</span>
         </div>
       }
     </li>
