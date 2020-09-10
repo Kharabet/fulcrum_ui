ebf69d6dbec96d658cf40619555cc08b40a58ab1
Rodion Kharabet <pensioneer4ik@gmail.com>
2020-09-10 20:12:39 +0300

    Torque: Add Legacy dApp link

--- packages/torque/src/layout/FooterMenu.tsx
+++ packages/torque/src/layout/FooterMenu.tsx
@@ -13,6 +13,9 @@ export class FooterMenu extends Component<IFooterMenuProps> {
     return (
       <div className="footer-menu">
         <div className="footer-menu__item">
+          <a href="https://legacy.torque.loans/">Legacy dApp</a>
+        </div>
+        <div className="footer-menu__item">
           <a href="https://torque.loans/tos/">Terms of use</a>
         </div>
         <div className="footer-menu__item">
