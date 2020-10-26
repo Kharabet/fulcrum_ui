d2caf95a12f0a31c17ce45b54942aa25d8de0946
Rodion Kharabet <pensioneer4ik@gmail.com>
2020-10-26 19:32:01 +0200

    fix(explorer): get rid of api calls for history prices on liquidations page.

--- packages/protocol-explorer/src/pages/LiquidationsPage.tsx
+++ packages/protocol-explorer/src/pages/LiquidationsPage.tsx
@@ -39,6 +39,7 @@ interface ILiquidationsPageState {
 export class LiquidationsPage extends Component<ILiquidationsPageProps, ILiquidationsPageState> {
   private _isMounted: boolean
 
+  private readonly stablecoins: Asset[] = [Asset.DAI, Asset.USDC, Asset.USDT]
   private readonly assetsShown: { token: Asset; color: string }[]
 
   constructor(props: any) {
@@ -228,16 +229,12 @@ export class LiquidationsPage extends Component<ILiquidationsPageProps, ILiquida
       )
       for (const e of tokenLiqudiations30d) {
         const loanAssetDecimals = AssetsDictionary.assets.get(e.loanToken)!.decimals || 18
-        const swapToUsdHistoryRateRequest = await fetch(
-          `https://api.bzx.network/v1/asset-history-price?asset=${
-            e.loanToken === Asset.fWETH ? 'eth' : e.loanToken.toLowerCase()
-          }&date=${e.timeStamp.getTime()}`
-        )
-        const swapToUsdHistoryRateResponse = (await swapToUsdHistoryRateRequest.json()).data
-        if (!swapToUsdHistoryRateResponse) continue
-        const repayAmountUsd = e.repayAmount
-          .div(10 ** loanAssetDecimals)
-          .times(swapToUsdHistoryRateResponse.swapToUSDPrice)
+        const collateralAssetDecimals =
+          AssetsDictionary.assets.get(e.collateralToken)!.decimals || 18
+        const swapToUSDPrice = this.stablecoins.includes(e.loanToken)
+          ? new BigNumber(1)
+          : new BigNumber(10 ** 36).div(e.collateralToLoanRate).div(10 ** collateralAssetDecimals)
+        const repayAmountUsd = e.repayAmount.div(10 ** loanAssetDecimals).times(swapToUSDPrice)
         volume30d = volume30d.plus(repayAmountUsd)
         liquidationEventsWithUsd.push({ event: e, repayAmountUsd: repayAmountUsd })
       }
