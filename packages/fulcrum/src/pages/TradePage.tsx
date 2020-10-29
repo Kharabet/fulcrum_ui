aba31c6bbda43c4fcf172cd089ba7b79c0d8ad49
Rodion Kharabet <pensioneer4ik@gmail.com>
2020-10-29 16:34:26 +0200

    revert "old" loans fix

--- packages/fulcrum/src/pages/TradePage.tsx
+++ packages/fulcrum/src/pages/TradePage.tsx
@@ -514,16 +514,12 @@ export default class TradePage extends PureComponent<ITradePageProps, ITradePage
       const depositAmount = loan.loanData.depositValue.div(10 ** loanAssetDecimals)
       const withdrawAmount = loan.loanData.withdrawalValue.div(10 ** loanAssetDecimals)
       profit =
-        depositAmount.gt(0) && withdrawAmount.gt(0)
-          ? estimatedCollateralReceived[1]
+        estimatedCollateralReceived[1]
               .div(10 ** collateralAssetDecimals)
               .times(currentCollateralToPrincipalRate)
               .minus(depositAmount)
               .plus(withdrawAmount)
-          : estimatedCollateralReceived[1]
-              .div(10 ** collateralAssetDecimals)
-              .times(currentCollateralToPrincipalRate)
-              .minus(deposited)
+          
     } else {
       collateral = collateralAssetAmount
 
@@ -572,13 +568,10 @@ export default class TradePage extends PureComponent<ITradePageProps, ITradePage
         .div(10 ** loanAssetDecimals)
         .div(currentCollateralToPrincipalRate)
 
-      profit =
-        depositAmount.gt(0) && withdrawAmount.gt(0)
-          ? estimatedCollateralReceived[1]
+      profit =estimatedCollateralReceived[1]
               .div(10 ** collateralAssetDecimals)
               .minus(depositAmount)
               .plus(withdrawAmount)
-          : estimatedCollateralReceived[1].div(10 ** collateralAssetDecimals).minus(deposited)
     }
 
     return {
