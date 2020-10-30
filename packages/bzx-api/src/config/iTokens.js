6f7d4a8a24e7870f3a5825b1b7941ff9afa61ef2
Rodion Kharabet <pensioneer4ik@gmail.com>
2020-10-30 19:35:18 +0200

    Add UNI and AAVE to bZx API

--- packages/bzx-api/src/config/iTokens.js
+++ packages/bzx-api/src/config/iTokens.js
@@ -94,5 +94,21 @@ export const iTokens = [
     displayName: 'YFI',
     iTokenName: 'iYFI',
     decimals: 18
+  },
+  {
+    name: 'uni',
+    address: '0x0a625fcec657053fe2d9fffdeb1dbb4e412cf8a8',
+    erc20Address: '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984',
+    displayName: 'UNI',
+    iTokenName: 'iUNI',
+    decimals: 18
+  },
+  {
+    name: 'aave',
+    address: '0x0cae8d91e0b1b7bd00d906e990c3625b2c220db1',
+    erc20Address: '0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9',
+    displayName: 'AAVE',
+    iTokenName: 'iAAVE',
+    decimals: 18
   }
 ]
