2d33cd53363caea1da1d49c16de48aca2b161371
OlehX <vovnyankoolehx@gmail.com>
2020-11-13 14:11:40 +0200

    Fulcrum: update tradeTokenGrid header

--- packages/fulcrum/src/components/TradeTokenGridHeader.tsx
+++ packages/fulcrum/src/components/TradeTokenGridHeader.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import ReactTooltip from 'react-tooltip'
 import { ReactComponent as IconInfo } from '../assets/images/icon_info.svg'
+import { Asset } from '../domain/Asset'
 import '../styles/components/tooltip.scss'
 
-export const TradeTokenGridHeader = () => {
+export interface ITradeTokenGridHeaderProps{
+  quoteToken: Asset|null
+}
+ 
+export const TradeTokenGridHeader = (props:ITradeTokenGridHeaderProps) => {
   const info =
     'Price feeds are provided securely via Chainlink, trades are executed via Kyber. This can result in minor price variations when opening and closing positions.'
 
@@ -20,13 +25,13 @@ export const TradeTokenGridHeader = () => {
       </div>
       <div className="trade-token-grid-header__col-price">
         <span className="trade-token-grid-header__text">
-          Mid Market Price
+          Mid Market Price <strong>{props.quoteToken}</strong>
           <IconInfo className="tooltip__icon" data-tip={info} />
           <ReactTooltip className="tooltip__info" place="top" effect="solid" />
         </span>
       </div>
       <div className="trade-token-grid-header__col-liquidation">
-        <span className="trade-token-grid-header__text">Liquidation Price</span>
+        <span className="trade-token-grid-header__text">Liquidation Price <strong>{props.quoteToken}</strong></span>
       </div>
       <div className="trade-token-grid-header__col-profit">
         <span className="trade-token-grid-header__text">Est. Yield, vBZRX</span>
