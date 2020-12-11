c0af3f33d5750faf935fe5803f7ed5432ac6098c
Rodion Kharabet <pensioneer4ik@gmail.com>
2020-12-11 16:34:41 +0200

    feat(torque): Use wallet provider for data fetching

--- packages/torque/src/components/AppRouter.tsx
+++ packages/torque/src/components/AppRouter.tsx
@@ -20,7 +20,6 @@ import Modal from 'react-modal'
 
 import { ProviderMenu } from './ProviderMenu'
 import { Web3ReactProvider } from '@web3-react/core'
-import { Web3ProviderEngine } from '@0x/subproviders'
 import { Web3ConnectionFactory } from '../domain/Web3ConnectionFactory'
 import { ProviderTypeDictionary } from '../domain/ProviderTypeDictionary'
 import { AbstractConnector } from '@web3-react/abstract-connector'
@@ -92,7 +91,7 @@ export class AppRouter extends Component<any, IAppRouterState> {
     this.doNetworkConnect()
   }
 
-  public getLibrary = async (provider: any, connector: any): Promise<Web3ProviderEngine> => {
+  public getLibrary = async (provider: any, connector: any): Promise<any> => {
     console.log(provider)
     //handle connectors events (i.e. network changed)
     await this.onProviderTypeSelect(connector)
