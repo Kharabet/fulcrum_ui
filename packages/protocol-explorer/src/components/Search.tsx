e57b94c599d25305785127ce938e730eaef3d71e
OlehX <vovnyankoolehx@gmail.com>
2020-12-17 12:54:37 +0200

    chore(explorer): refactor search component

--- packages/protocol-explorer/src/components/Search.tsx
+++ packages/protocol-explorer/src/components/Search.tsx
@@ -1,9 +1,10 @@
-import React, { ChangeEvent, Component, FormEvent } from 'react'
+import React, { ChangeEvent, Component, FormEvent, useState } from 'react'
 
 import { ReactComponent as IconClear } from '../assets/images/icon-form-clear.svg'
 import { ReactComponent as IconSearch } from '../assets/images/icon-form-search.svg'
 interface ISearchProps {
   initialFilter?: string
+  children?: string
   onSearch: (filter: string) => void
 }
 
@@ -12,64 +13,57 @@ interface ISearchState {
   inputValue: string
 }
 
-export class Search extends Component<ISearchProps, ISearchState> {
-  private _input: React.RefObject<HTMLInputElement>
-  constructor(props: any) {
-    super(props)
-    this._input = React.createRef()
-    this.state = {
-      onFocus: false,
-      inputValue: props.initialFilter || ''
-    }
+export const Search = (props: ISearchProps) => {
+  const _input: React.RefObject<HTMLInputElement> = React.createRef()
+  const [isFocus, setIsFocus] = useState(false)
+  const [inputValue, setInputValue] = useState<string>(props.initialFilter || '')
+
+  const onFocus = () => {
+    setIsFocus(true)
   }
-  public render() {
-    return (
-      <React.Fragment>
-        <form className={`search ${this.state.onFocus ? `focus` : ``}`}>
-          <input
-            ref={this._input}
-            placeholder="Search"
-            onFocus={this.onFocus}
-            onBlur={this.onBlur}
-            onChange={this.onChange}
-            value={this.state.inputValue}
-          />
-          {this.state.inputValue.length > 0 && (
-            <button onClick={this.resetInput}>
-              <IconClear />
-            </button>
-          )}
-          {this.state.inputValue.length === 0 && (
-            <button onClick={this.onSearchClick}>
-              {' '}
-              <IconSearch />
-            </button>
-          )}
-        </form>
-        <p>Enter transaction hash or user address </p>
-      </React.Fragment>
-    )
-  }
-  public onFocus = () => {
-    this.setState({ ...this.state, onFocus: true })
-  }
-  public onBlur = () => {
-    this.setState({ ...this.state, onFocus: false })
+  const onBlur = () => {
+    setIsFocus(false)
   }
 
-  public onSearchClick = (event: FormEvent) => {
+  const onSearchClick = (event: FormEvent) => {
     event.preventDefault()
-
-    this._input.current && this._input.current.focus()
+    _input.current && _input.current.focus()
   }
 
-  public onChange = (event: ChangeEvent<HTMLInputElement>) => {
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
     const value = event.target.value ? event.target.value : ''
-    this.setState({ ...this.state, inputValue: value })
-    this.props.onSearch(value.toLowerCase())
+    setInputValue(value)
+    props.onSearch(value.toLowerCase())
   }
-  public resetInput = () => {
-    this.setState({ ...this.state, inputValue: '' })
-    this.props.onSearch('')
+  const resetInput = () => {
+    setInputValue('')
+    props.onSearch('')
   }
+
+  return (
+    <React.Fragment>
+      <form className={`search ${isFocus ? `focus` : ``}`}>
+        <input
+          ref={_input}
+          placeholder="Search"
+          onFocus={onFocus}
+          onBlur={onBlur}
+          onChange={onChange}
+          value={inputValue}
+        />
+        {inputValue.length > 0 && (
+          <button onClick={resetInput}>
+            <IconClear />
+          </button>
+        )}
+        {inputValue.length === 0 && (
+          <button onClick={onSearchClick}>
+            {' '}
+            <IconSearch />
+          </button>
+        )}
+      </form>
+      <p>{props.children || 'Enter transaction hash or user address'} </p>
+    </React.Fragment>
+  )
 }
