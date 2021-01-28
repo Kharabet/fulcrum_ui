f84105b523188b2f1e115eac86cafb714da656ad
Kevin Purnelle <kevin.purnelle@gmail.com>
2021-01-28 16:08:59 +0100

    refactor(staking): reuse code for user balances view

--- packages/staking-dashboard/src/App/Staking/StakingForm/UserBalances/UserBalances.tsx
+++ packages/staking-dashboard/src/App/Staking/StakingForm/UserBalances/UserBalances.tsx
@@ -1,14 +1,13 @@
-import { tokenIcons } from 'app-images'
 import { observer } from 'mobx-react'
 import React from 'react'
-import AssetBalance from 'shared-components/AssetBalance'
 import RootStore from 'src/stores/RootStore'
 import { Loader } from 'ui-framework'
+import Balances from './Balances'
 
 export function UserBalances({ rootStore }: { rootStore: RootStore }) {
   const { stakingStore, etherscanURL } = rootStore
   const { userBalances } = stakingStore
-  const { wallet, staked } = stakingStore.userBalances
+  const { wallet, staked } = userBalances
 
   return (
     <div>
@@ -24,73 +23,16 @@ export function UserBalances({ rootStore }: { rootStore: RootStore }) {
             />
           )}
           <h3 className="section-header">Wallet Balance</h3>
-          <AssetBalance
-            className="margin-bottom-2"
-            link={`${etherscanURL}token/0x56d811088235F11C8920698a204A5010a788f4b3`}
-            tokenLogo={tokenIcons.bzrx}
-            balance={wallet.bzrx}
-            name="BZRX"
-          />
-
-          <AssetBalance
-            className="margin-bottom-2"
-            link={`${etherscanURL}token/0xB72B31907C1C95F3650b64b2469e08EdACeE5e8F`}
-            tokenLogo={tokenIcons.vbzrx}
-            balance={wallet.vbzrx}
-            name="vBZRX"
-          />
-
-          <AssetBalance
-            className="margin-bottom-2"
-            link={`${etherscanURL}token/0x18240BD9C07fA6156Ce3F3f61921cC82b2619157`}
-            tokenLogo={tokenIcons.ibzrx}
-            balance={wallet.ibzrx}
-            name="iBZRX"
-          />
-
-          <AssetBalance
-            link={`${etherscanURL}token/0xe26A220a341EAca116bDa64cF9D5638A935ae629`}
-            tokenLogo={tokenIcons.bpt}
-            balance={wallet.bpt}
-            name="BPT"
-          />
+          <Balances wallet={wallet} etherscanURL={etherscanURL} />
         </div>
+
         <div className="panel--white padded-2">
           <h3 className="section-header">Staking Balance</h3>
-
-          <AssetBalance
-            className="margin-bottom-2"
-            link={`${etherscanURL}token/0x56d811088235F11C8920698a204A5010a788f4b3`}
-            tokenLogo={tokenIcons.bzrx}
-            balance={staked.bzrx}
-            name="BZRX"
-          />
-
-          <AssetBalance
-            className="margin-bottom-2"
-            link={`${etherscanURL}token/0xB72B31907C1C95F3650b64b2469e08EdACeE5e8F`}
-            tokenLogo={tokenIcons.vbzrx}
-            balance={staked.vbzrx}
-            name="vBZRX"
-          />
-
-          <AssetBalance
-            className="margin-bottom-2"
-            link={`${etherscanURL}token/0x18240BD9C07fA6156Ce3F3f61921cC82b2619157`}
-            tokenLogo={tokenIcons.ibzrx}
-            balance={staked.ibzrx}
-            name="iBZRX"
-          />
-
-          <AssetBalance
-            link={`${etherscanURL}token/0xe26A220a341EAca116bDa64cF9D5638A935ae629`}
-            tokenLogo={tokenIcons.bpt}
-            balance={staked.bpt}
-            name="BPT"
-          />
+          <Balances wallet={staked} etherscanURL={etherscanURL} />
         </div>
       </div>
-      {userBalances.loaded && userBalances.wallet.isEmpty && userBalances.staked.isEmpty && (
+
+      {userBalances.loaded && wallet.isEmpty && staked.isEmpty && (
         <p>
           <i>There are no tokens available to stake in your wallet.</i>
         </p>
