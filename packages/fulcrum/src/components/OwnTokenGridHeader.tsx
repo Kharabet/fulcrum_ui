aab510b5190d8c61a2a3ed4697e571cd1ba0d36b
Diana Kovalyova <dianaborysivna@gmail.com>
2021-01-29 17:13:50 +0200

    Add tooltip profit for open positions tab and animation for switch

--- packages/fulcrum/src/components/OwnTokenGridHeader.tsx
+++ packages/fulcrum/src/components/OwnTokenGridHeader.tsx
@@ -3,6 +3,8 @@ import ReactTooltip from 'react-tooltip'
 import { ReactComponent as IconInfo } from '../assets/images/icon_info.svg'
 
 const OwnTokenGridHeader = () => {
+  const tooltipProfit =
+    '<p class="tooltip__info__profit__title">Profit is displayed in two ways:</p><ul class="tooltip__info__profit__ul"><li>Left: value of traded asset</li><li>Right: value of total position profit including collateral change </li></ul>'
   return (
     <div className="own-token-grid-header">
       <div className="own-token-grid-header__col-token-name">
@@ -29,6 +31,14 @@ const OwnTokenGridHeader = () => {
       </div>
       <div className="own-token-grid-header__col-profit">
         <span className="own-token-grid-header__text">Profit</span>
+        <IconInfo
+          className="tooltip__icon"
+          data-tip={tooltipProfit}
+          data-multiline="true"
+          data-html={true}
+          data-for="profit-tooltip"
+        />
+        <ReactTooltip id="profit-tooltip" className="tooltip__info" place="top" effect="solid" />
       </div>
     </div>
   )
