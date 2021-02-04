7433655b62c5d1ced865f3b77da1cff0ff60e4cb
Rodion Kharabet <pensioneer4ik@gmail.com>
2021-02-04 15:20:35 +0200

    chore(staking): Don't use modal for proposal details

--- packages/staking-dashboard/src/App/Staking/Governance/Governance.tsx
+++ packages/staking-dashboard/src/App/Staking/Governance/Governance.tsx
@@ -6,38 +6,29 @@ import GovernanceItem from './GovernanceItem'
 
 export default function Governance({ vm }: { vm: GovernanceVM }) {
   const governanceProposals = vm.governanceStore.proposalsList.map((proposal) => (
-    <GovernanceItem
-      key={proposal.id}
-      proposal={proposal}
-      openProposals={vm.showProposal}
-    />
+    <GovernanceItem key={proposal.id} proposal={proposal} openProposals={vm.showProposal} />
   ))
 
-  return (
-    <React.Fragment>
-      <Proposals vm={vm} />
-      <div className="panel--white padded-2 margin-bottom-2 governance">
-        <div className="governance__search">
-          <input
-            value={vm.name}
-            placeholder="Search"
-            onChange={(e) => (vm.name = e.target.value)}
-          />
-          <div className="governance__search__button" onClick={vm.search}>
-            <SearchIcon />
-          </div>
-        </div>
-        <div className="governance__table">
-          <div className="thead">
-            <div className="thead__description">Description</div>
-            <div className="thead__right">
-              <div className="thead__author">Author</div>
-              <div className="thead__action">Action</div>
-            </div>
-          </div>
-          <div className="tbody">{governanceProposals}</div>
+  return vm.proposalPopup.visible ? (
+    <Proposals vm={vm} />
+  ) : (
+    <div className="panel--white padded-2 margin-bottom-2 governance">
+      <div className="governance__search">
+        <input value={vm.name} placeholder="Search" onChange={(e) => (vm.name = e.target.value)} />
+        <div className="governance__search__button" onClick={vm.search}>
+          <SearchIcon />
         </div>
       </div>
-    </React.Fragment>
+      <div className="governance__table">
+        <div className="thead">
+          <div className="thead__description">Description</div>
+          <div className="thead__right">
+            <div className="thead__author">Author</div>
+            <div className="thead__action">Action</div>
+          </div>
+        </div>
+        <div className="tbody">{governanceProposals}</div>
+      </div>
+    </div>
   )
 }
--- packages/staking-dashboard/src/App/Staking/Governance/Proposals/Proposals.tsx
+++ packages/staking-dashboard/src/App/Staking/Governance/Proposals/Proposals.tsx
@@ -1,5 +1,7 @@
 import 'simplebar/dist/simplebar.min.css'
 import { ReactComponent as CloseIcon } from 'app-images/ic__close.svg'
+import { ReactComponent as ArrowLeftIcon } from 'app-images/ic_arrow_left.svg'
+
 import Markdown from 'markdown-to-jsx'
 import { observer } from 'mobx-react'
 import React from 'react'
@@ -17,78 +19,65 @@ export function Proposals({ vm }: { vm: GovernanceVM }) {
     return null
   }
   return (
-    <Modal
-      isOpen={proposalPopup.visible && activeProposal !== undefined}
-      className="modal__content"
-      overlayClassName="modal__overlay"
-      ariaHideApp={false}>
-      <SimpleBar style={{ maxHeight: '90vh', width: '720px', margin: '10px 0' }}>
-        <div className="proposals">
-          <div className="proposals-header">
-            <span>{activeProposal.title}</span>
-            <div onClick={proposalPopup.hide}>
-              <CloseIcon className="disclosure__close" />
-            </div>
+    <div className="proposals">
+      <div className="proposals-header">
+        <span className="back-to-list" onClick={proposalPopup.hide}>
+          <ArrowLeftIcon />
+        </span>
+        &nbsp;<span>{activeProposal.title}</span>
+      </div>
+      <div className="proposals-body">
+        <div className="proposals-votes">
+          <div className="proposals-votes__title">Votes</div>
+          <div className="proposals-votes__range">
+            <div
+              className="proposals-votes__range-active"
+              style={{
+                width: `${activeProposal.votesFor
+                  .div(activeProposal.votesFor.plus(activeProposal.votesAgainst))
+                  .times(100)
+                  .toNumber()}%`
+              }}
+            />
           </div>
-          <div className="proposals-body">
-            <div className="proposals-votes">
-              <div className="proposals-votes__title">Votes</div>
-              <div className="proposals-votes__range">
-                <div
-                  className="proposals-votes__range-active"
-                  style={{
-                    width: `${activeProposal.votesFor
-                      .div(activeProposal.votesFor.plus(activeProposal.votesAgainst))
-                      .times(100)
-                      .toNumber()}%`
-                  }}
-                />
-              </div>
-              <div className="proposals-votes__data">
-                <div>
-                  <label>For:&nbsp;</label>
-                  <span className="value">{activeProposal.votesFor.toFixed(0)}</span>
-                </div>
-                <div>
-                  <label>Against:&nbsp;</label>
-                  <span className="value">{activeProposal.votesAgainst.toFixed(0)}</span>
-                </div>
-              </div>
+          <div className="proposals-votes__data">
+            <div>
+              <label>For:&nbsp;</label>
+              <span className="value">{activeProposal.votesFor.toFixed(0)}</span>
             </div>
-            <div className="flex jc-sb">
-              <div className="proposals-actions">
-                <div className="proposals-actions__title">Actions</div>
-                <div className="proposals-actions__details">
-                  {activeProposal.actions.map((item, index) => {
-                    return <ProposalActionItem key={index} index={index} {...item} />
-                  })}
-                </div>
-              </div>
-              <div className="proposals-history">
-                <div className="proposals-history__title">Proposal History</div>
-                <ul className="proposals-history__list">
-                  {activeProposal.history.map((item, index) => {
-                    return <ProposalHistoryItem key={index} etherscanURL={etherscanURL} {...item} />
-                  })}
-                </ul>
-              </div>
+            <div>
+              <label>Against:&nbsp;</label>
+              <span className="value">{activeProposal.votesAgainst.toFixed(0)}</span>
             </div>
-
-            <div className="proposals-info">
-              <div className="proposals-info__title">Info</div>
-              <div className="proposals-info__description">
-                <Markdown>{activeProposal.description}</Markdown>
-              </div>
-            </div>
-          </div>
-          <div className="flex jc-fe">
-            <button className="button blue proposals-button" onClick={proposalPopup.hide}>
-              Cancel
-            </button>
           </div>
         </div>
-      </SimpleBar>
-    </Modal>
+        <div className="flex jc-sb">
+          <div className="proposals-actions">
+            <div className="proposals-actions__title">Actions</div>
+            <div className="proposals-actions__details">
+              {activeProposal.actions.map((item, index) => {
+                return <ProposalActionItem key={index} index={index} {...item} />
+              })}
+            </div>
+          </div>
+          <div className="proposals-history">
+            <div className="proposals-history__title">Proposal History</div>
+            <ul className="proposals-history__list">
+              {activeProposal.history.map((item, index) => {
+                return <ProposalHistoryItem key={index} etherscanURL={etherscanURL} {...item} />
+              })}
+            </ul>
+          </div>
+        </div>
+
+        <div className="proposals-info">
+          <div className="proposals-info__title">Info</div>
+          <div className="proposals-info__description">
+            <Markdown>{activeProposal.description}</Markdown>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
