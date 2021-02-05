f5dc54ddce9d57dccc46cf1b9720b6ffce655c50
Rodion Kharabet <pensioneer4ik@gmail.com>
2021-02-05 12:50:04 +0200

    chore(fulcrum): reduce recursive calls number for positions fetching (from 10 to 3)

--- packages/fulcrum/src/pages/TradePage.tsx
+++ packages/fulcrum/src/pages/TradePage.tsx
@@ -182,7 +182,7 @@ export default class TradePage extends PureComponent<ITradePageProps, ITradePage
     await this.getTokenRowsData()
     await this.getInnerOwnRowsData()
     await this.setRecentLiquidationsNumber()
-    await this.fetchPositionsRecursive(10)
+    await this.fetchPositionsRecursive(3)
     setTimeout(() => {
       this.forceUpdate() // solves bug with positions not appearing on the first render.
     }, 5000)
