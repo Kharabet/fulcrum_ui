875a6f72c12d016d0f1bc6899c09e0f83fc775d1
Rodion Kharabet <pensioneer4ik@gmail.com>
2021-02-17 23:50:18 +0200

    fix(fulcrum): Move TRADE_PAIRS to app config. Apply it to determine position type at history tab

--- packages/fulcrum/src/pages/TradePage.tsx
+++ packages/fulcrum/src/pages/TradePage.tsx
@@ -19,7 +19,7 @@ import { ExtendLoanForm } from '../components/ExtendLoanForm'
 import Asset from 'bzx-common/src/assets/Asset'
 
 import AssetsDictionary from 'bzx-common/src/assets/AssetsDictionary'
-
+import { TRADE_PAIRS } from '../config/appConfig'
 import {
   CloseWithSwapEvent,
   DepositCollateralEvent,
@@ -85,56 +85,6 @@ interface ITradePageState {
   isSupportNetwork: boolean
 }
 
-const TRADE_PAIRS: { baseToken: Asset; quoteToken: Asset }[] =
-  (process.env.REACT_APP_ETH_NETWORK === 'mainnet' && [
-    { baseToken: Asset.ETH, quoteToken: Asset.DAI },
-    { baseToken: Asset.ETH, quoteToken: Asset.USDC },
-    { baseToken: Asset.ETH, quoteToken: Asset.USDT },
-    { baseToken: Asset.ETH, quoteToken: Asset.WBTC },
-    { baseToken: Asset.ETH, quoteToken: Asset.BZRX },
-    { baseToken: Asset.DAI, quoteToken: Asset.USDC },
-    { baseToken: Asset.DAI, quoteToken: Asset.USDT },
-    { baseToken: Asset.WBTC, quoteToken: Asset.DAI },
-    { baseToken: Asset.WBTC, quoteToken: Asset.USDC },
-    { baseToken: Asset.WBTC, quoteToken: Asset.USDT },
-    { baseToken: Asset.WBTC, quoteToken: Asset.BZRX },
-    { baseToken: Asset.BZRX, quoteToken: Asset.DAI },
-    { baseToken: Asset.BZRX, quoteToken: Asset.USDC },
-    { baseToken: Asset.BZRX, quoteToken: Asset.USDT },
-    { baseToken: Asset.LINK, quoteToken: Asset.DAI },
-    { baseToken: Asset.LINK, quoteToken: Asset.USDC },
-    { baseToken: Asset.LINK, quoteToken: Asset.USDT },
-    { baseToken: Asset.LINK, quoteToken: Asset.BZRX },
-    { baseToken: Asset.MKR, quoteToken: Asset.DAI },
-    { baseToken: Asset.MKR, quoteToken: Asset.USDC },
-    { baseToken: Asset.MKR, quoteToken: Asset.USDT },
-    { baseToken: Asset.MKR, quoteToken: Asset.BZRX },
-    { baseToken: Asset.YFI, quoteToken: Asset.DAI },
-    { baseToken: Asset.YFI, quoteToken: Asset.USDC },
-    { baseToken: Asset.YFI, quoteToken: Asset.USDT },
-    { baseToken: Asset.YFI, quoteToken: Asset.BZRX },
-    { baseToken: Asset.KNC, quoteToken: Asset.DAI },
-    { baseToken: Asset.KNC, quoteToken: Asset.USDC },
-    { baseToken: Asset.KNC, quoteToken: Asset.USDT },
-    { baseToken: Asset.KNC, quoteToken: Asset.BZRX },
-    { baseToken: Asset.UNI, quoteToken: Asset.DAI },
-    { baseToken: Asset.UNI, quoteToken: Asset.USDC },
-    { baseToken: Asset.UNI, quoteToken: Asset.USDT },
-    { baseToken: Asset.UNI, quoteToken: Asset.BZRX },
-    { baseToken: Asset.AAVE, quoteToken: Asset.DAI },
-    { baseToken: Asset.AAVE, quoteToken: Asset.USDC },
-    { baseToken: Asset.AAVE, quoteToken: Asset.USDT },
-    { baseToken: Asset.AAVE, quoteToken: Asset.BZRX },
-    { baseToken: Asset.LRC, quoteToken: Asset.DAI },
-    { baseToken: Asset.LRC, quoteToken: Asset.USDC },
-    { baseToken: Asset.LRC, quoteToken: Asset.USDT },
-    { baseToken: Asset.LRC, quoteToken: Asset.BZRX },
-    { baseToken: Asset.COMP, quoteToken: Asset.DAI },
-    { baseToken: Asset.COMP, quoteToken: Asset.USDC },
-    { baseToken: Asset.COMP, quoteToken: Asset.USDT },
-    { baseToken: Asset.COMP, quoteToken: Asset.BZRX }
-  ]) ||
-  []
 
 export default class TradePage extends PureComponent<ITradePageProps, ITradePageState> {
   private _isMounted: boolean = false
@@ -365,8 +315,8 @@ export default class TradePage extends PureComponent<ITradePageProps, ITradePage
             {this.state.activeTokenGridTab === TokenGridTab.Open && (
               <OwnTokenGrid
                 ownRowsData={this.state.ownRowsData}
-                isMobileMedia={this.props.isMobileMedia}                
-                onStartTrading={()=>this.onTokenGridTabChange(TokenGridTab.Chart)}
+                isMobileMedia={this.props.isMobileMedia}
+                onStartTrading={() => this.onTokenGridTabChange(TokenGridTab.Chart)}
               />
             )}
 
@@ -380,7 +330,7 @@ export default class TradePage extends PureComponent<ITradePageProps, ITradePage
                 quoteTokens={this.quoteTokens}
                 updateHistoryRowsData={this.updateHistoryRowsData}
                 changeLoadingTransaction={this.changeLoadingTransaction}
-                onStartTrading={()=>this.onTokenGridTabChange(TokenGridTab.Chart)}
+                onStartTrading={() => this.onTokenGridTabChange(TokenGridTab.Chart)}
               />
             )}
 
