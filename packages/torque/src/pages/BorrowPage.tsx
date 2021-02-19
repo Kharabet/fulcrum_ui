2c82096379be2d5acc5ed3277749bd5383a7176b
OlehX <vovnyankoolehx@gmail.com>
2021-02-19 16:49:02 +0200

    chore(torque): add redirect to dashboard after borrow

--- packages/torque/src/pages/BorrowPage.tsx
+++ packages/torque/src/pages/BorrowPage.tsx
@@ -5,6 +5,7 @@ import { TorqueProvider } from '../services/TorqueProvider'
 
 export interface IBorrowPageProps {
   doNetworkConnect: () => void
+  setLoansActiveTab:()=> void
   isMobileMedia: boolean
 }
 
@@ -16,7 +17,7 @@ const BorrowPage = (props: IBorrowPageProps) => {
       <BorrowDlg ref={borrowDlgRef} />
       <div>
         {!TorqueProvider.Instance.unsupportedNetwork ? (
-          <AssetSelector borrowDlgRef={borrowDlgRef} doNetworkConnect={props.doNetworkConnect} />
+          <AssetSelector borrowDlgRef={borrowDlgRef} doNetworkConnect={props.doNetworkConnect} setLoansActiveTab={props.setLoansActiveTab}/>
         ) : (
           <div style={{ textAlign: `center`, fontSize: `2rem`, paddingBottom: `1.5rem` }}>
             <div style={{ cursor: `pointer` }}>You are connected to the wrong network.</div>
