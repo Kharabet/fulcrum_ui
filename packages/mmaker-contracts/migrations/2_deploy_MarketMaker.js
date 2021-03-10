8486129b57810cd263017021b383537cdb82f918
Kevin Purnelle <kevin.purnelle@gmail.com>
2021-03-10 17:14:29 +0100

    refactor(mmaker): delete inactive project
    backup in backup/mmaker branch if needed

--- packages/mmaker-contracts/migrations/2_deploy_MarketMaker.js
+++
@@ -1,8 +0,0 @@
-const MarketMaker = artifacts.require("MarketMaker");
-
-module.exports = (deployer) => {
-  deployer.then(async () => {
-    await deployer.deploy(MarketMaker, []);
-    console.log(`   > MarketMaker deploy: #done`);
-  });
-};
